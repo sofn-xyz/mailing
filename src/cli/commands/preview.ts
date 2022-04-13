1e708b8f0d46faf708fb01454b4be6ba6a0ee386
Peter Sugihara <peter@campsh.com>
2022-04-13 10:12:06 -0700

    init template

--- src/cli/commands/preview.ts
+++ src/cli/commands/preview.ts
@@ -1,15 +1,18 @@
+const DEFAULT_PORT = 3883;
+
 exports.command = "preview";
 
 exports.describe = "start the email preview server";
 
 exports.builder = {
   port: {
-    default: 3883,
+    default: DEFAULT_PORT,
   },
 };
 
 type ArgV = { port: number };
 
 exports.handler = (argv?: ArgV) => {
-  console.log("implement me!", argv?.port);
+  const port = argv?.port || DEFAULT_PORT;
+  console.log("implement me!", port);
 };
