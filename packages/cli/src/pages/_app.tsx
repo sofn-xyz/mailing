896d5e5815fcc682d9587ec6d9805eb0a7848ed6
Peter Sugihara <peter@campsh.com>
2022-07-08 10:32:49 -0700

    fix reset

--- packages/cli/src/pages/_app.tsx
+++ packages/cli/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "../reset.css";
-import "../global.css";
+import "../globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
