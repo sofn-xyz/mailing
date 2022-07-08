075428e8712590240f88ec3d299f14f9a4650e93
Peter Sugihara <peter@campsh.com>
2022-07-08 12:37:27 -0700

    remove console.log

--- packages/cli/src/pages/index.tsx
+++ packages/cli/src/pages/index.tsx
@@ -52,7 +52,6 @@ const Home: React.FC<HomeProps> = ({ previews }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  console.log(context.resolvedUrl);
   const previews = await fetch("http://localhost:3883/previews.json");
   return { props: { previews: await previews.json() } };
 };
