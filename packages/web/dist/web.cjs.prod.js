b7bbc7e9f68e84e06ffbaa0944f41ada386d1a49
Peter Sugihara <peter@campsh.com>
2022-07-16 12:19:05 -0700

    add web package

---
+++ packages/web/dist/web.cjs.prod.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+console.log("hello");
+var index = {};
+
+exports["default"] = index;
---
+++ packages/web/src/index.js
@@ -0,0 +1,5 @@
+import mailing from "mailing";
+
+console.log("hello");
+
+export default {};
