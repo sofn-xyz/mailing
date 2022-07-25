9fb3eae23a878128d93e1aea7e33a4fc5647918d
Alex Farrill <alex.farrill@gmail.com>
2022-07-25 14:02:13 -0700

    Revert "try swc"
    This reverts commit 09757e63155bbc0f7af2328a08eae82dc06d6b35.

--- packages/cli/dist/mailing.cjs.prod.js
+++ packages/cli/dist/mailing.cjs.prod.js
@@ -852,7 +852,9 @@ var handler$1 = /*#__PURE__*/function () {
               }
             });
 
-            require("@swc/register");
+            require("@babel/register")({
+              presets: ["@babel/preset-react"]
+            });
 
             port = (argv === null || argv === void 0 ? void 0 : argv.port) || DEFAULT_PORT;
             dev = !!process.env.MM_DEV;
