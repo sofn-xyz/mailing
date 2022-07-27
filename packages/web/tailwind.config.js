a737f3277110894a46c89b15959f11d2d23fa72a
Monica Kogler <monica@campsh.com>
2022-07-26 20:52:45 -0700

    basic layout

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: "#fafa98",
+      },
+    },
   },
   plugins: [],
 };
