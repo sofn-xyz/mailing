3b221c957e7e4245fcdb77ab75273e56c660b3bd
Peter Sugihara <peter@campsh.com>
2022-08-01 22:47:39 -0700

    fetch localhost if on server

--- packages/cli/src/pages/index.tsx
+++ packages/cli/src/pages/index.tsx
@@ -141,10 +141,10 @@ const Home: NextPage<{ previews: [string, string[]][] }> = ({ previews }) => {
 Home.getInitialProps = async (context: NextPageContext) => {
   let previews: [string, string[]][] = [];
 
-  try {
+  if (context.req) {
     const res = await fetch("http://localhost:3883/previews.json");
     previews = await res.json();
-  } catch (e) {}
+  }
 
   return { previews };
 };
