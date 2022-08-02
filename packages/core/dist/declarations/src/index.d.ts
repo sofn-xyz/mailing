5ab287480d8bf375ddc8efaa712812931dd004ff
Peter Sugihara <peter@campsh.com>
2022-08-02 13:31:56 -0700

    build

--- packages/core/dist/declarations/src/index.d.ts
+++ packages/core/dist/declarations/src/index.d.ts
@@ -1,5 +1,6 @@
 import { ReactElement, JSXElementConstructor } from "react";
 import nodemailer from "nodemailer";
+import { render } from "./mjml";
 export declare type ComponentMail = nodemailer.SendMailOptions & {
     component: ReactElement<any, string | JSXElementConstructor<any>>;
     forceDeliver?: boolean;
@@ -12,7 +13,4 @@ export declare type BuildSendMailOptions = {
 export declare function getTestMailQueue(): Promise<any>;
 export declare function clearTestMailQueue(): Promise<void>;
 export declare function buildSendMail(options: BuildSendMailOptions): (mail: ComponentMail) => Promise<any>;
-declare const _default: {
-    buildSendMail: typeof buildSendMail;
-};
-export default _default;
+export { buildSendMail as default, render };
