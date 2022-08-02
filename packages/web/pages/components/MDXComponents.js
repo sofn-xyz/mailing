bac1ad3296570c35a91f2d4e8ceb843ecde79837
Alex Farrill <alex.farrill@gmail.com>
2022-08-02 15:51:20 -0700

    some tailwind styles for blog

--- packages/web/pages/components/MDXComponents.js
+++ packages/web/pages/components/MDXComponents.js
@@ -1,5 +1,12 @@
 const MDXComponents = {
-  h1: (props) => <h1 {...props} className="font-bold" />,
+  h1: (props) => <h1 {...props} className="text-3xl font-bold py-2" />,
+  p: (props) => <p {...props} className="py-1" />,
+  code: (props) => (
+    <code
+      {...props}
+      className="block font-mono bg-slate-700 p-3 my-3 text-gray-100"
+    />
+  ),
 };
 
 export default MDXComponents;
