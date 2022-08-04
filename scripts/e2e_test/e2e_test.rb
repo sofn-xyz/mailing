581c8e424ff477dde672d05252359928ac32a51e
Alex Farrill <alex.farrill@gmail.com>
2022-08-04 13:45:49 -0700

    e2e test check-in

---
+++ scripts/e2e_test/e2e_test.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'tmpdir'
+
+class TestRunner
+  BASE_DIR = "./runs"
+  NUM_RUNS_TO_KEEP = 5
+
+  E2E_CONFIG = [
+    {name: 'next_ts',
+      commands: [
+        "yarn create next-app . --typescript",
+        "yalc add mailing",
+        "npx mailing --quiet"
+      ]},
+    # {name: 'next_js'},
+  ];
+
+    def run
+      Dir.chdir("../..") do
+        system("yalc add")
+        system("yarn build")
+        system("yalc push")
+      end
+
+      E2E_CONFIG.each do |config|
+        begin
+          tmp_dir_name = BASE_DIR + "/" + [config[:name], Time.now.strftime("%Y%m%d%H%M%S")].join('-')
+
+          puts "⚙️  " * 10
+          puts "Attempting #{config[:name]} in #{tmp_dir_name}"
+          puts "⚙️  " * 10
+          
+          Dir.mkdir(tmp_dir_name)
+
+          Dir.chdir(tmp_dir_name) do
+            config[:commands].each do |cmd|
+              puts "running #{cmd}"
+              system(cmd)
+            end
+          end
+        ensure
+          cleanup_runs_directory
+        end
+      end
+    end
+
+    def cleanup_runs_directory
+      Dir.glob("#{BASE_DIR}/*")[NUM_RUNS_TO_KEEP..-1]&.sort{|a,b| b <=> a}&.each do |dir|
+        puts "Cleaning up #{dir}"
+        FileUtils.rm_rf(dir)
+      end
+    end
+  end
+
+  TestRunner.new.run
