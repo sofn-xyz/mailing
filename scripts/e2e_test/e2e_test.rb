a7932a7087bd1271be53d278030e51b8a8deeb0c
Alex Farrill <alex.farrill@gmail.com>
2022-08-05 14:36:23 -0700

    working cypress test

--- scripts/e2e_test/e2e_test.rb
+++ scripts/e2e_test/e2e_test.rb
@@ -5,6 +5,7 @@ require 'tmpdir'
 class TestRunner
   BASE_DIR = "./runs"
   NUM_RUNS_TO_KEEP = 5
+  CYPRESS_DIR = File.expand_path(__dir__ + '/../../packages/cli/cypress')
 
   E2E_CONFIG = [
     {name: 'next_ts',
@@ -15,53 +16,71 @@ class TestRunner
     # {name: 'next_js'},
   ];
 
-    def run
-      Dir.chdir("../..") do
-        system("yalc add")
-        system("yarn build")
-        system("yalc push")
-      end
+  def initialize
+    fail "Check that CYPRESS_DIR exists: #{CYPRESS_DIR}" unless Dir.exists?(CYPRESS_DIR)
+  end
 
-      E2E_CONFIG.each do |config|
-        begin
-          tmp_dir_name = BASE_DIR + "/" + [config[:name], Time.now.strftime("%Y%m%d%H%M%S")].join('-')
-
-          puts "⚙️  " * 10
-          puts "Attempting #{config[:name]} in #{tmp_dir_name}"
-          puts "⚙️  " * 10
-          
-          Dir.mkdir(tmp_dir_name)
-
-          Dir.chdir(tmp_dir_name) do
-            config[:commands].each do |cmd|
-              puts "running #{cmd}"
-              system(cmd)
-            end
-
-            # open the subprocess
-            f = IO.popen("npx mailing --typescript --emails-dir=\"./emails\"")
-
-            # run cypress tests
-            # implement me
-
-            # kill the subprocess
-            Process.kill "INT", f.pid
-
-            # close the io
-            f.close
-          end
-        ensure
-          cleanup_runs_directory
-        end
-      end
+  def run
+    Dir.chdir("../..") do
+      system("yalc add")
+      system("yarn build")
+      system("yalc push")
     end
 
-    def cleanup_runs_directory
-      Dir.glob("#{BASE_DIR}/*")[NUM_RUNS_TO_KEEP..-1]&.sort{|a,b| b <=> a}&.each do |dir|
-        puts "Cleaning up #{dir}"
-        FileUtils.rm_rf(dir)
+    E2E_CONFIG.each do |config|
+      begin
+        tmp_dir_name = BASE_DIR + "/" + [config[:name], Time.now.strftime("%Y%m%d%H%M%S")].join('-')
+
+        puts "⚙️  " * 10
+        puts "Attempting #{config[:name]} in #{tmp_dir_name}"
+        puts "⚙️  " * 10
+        
+        Dir.mkdir(tmp_dir_name)
+
+        Dir.chdir(tmp_dir_name) do
+          config[:commands].each do |cmd|
+            puts "running #{cmd}"
+            system(cmd)
+          end
+
+          # open the subprocess
+          @io = IO.popen("npx mailing --typescript --emails-dir=\"./emails\"")
+          # should probably wait for something here
+          
+        end
+
+        # run cypress tests
+        run_cypress_tests
+      ensure
+        cleanup_io_and_subprocess
+        cleanup_runs_directory
       end
     end
   end
 
-  TestRunner.new.run
+  def run_cypress_tests
+    Dir.chdir(CYPRESS_DIR) do
+      system("yarn cypress run")
+    end
+  end
+
+  def cleanup_io_and_subprocess
+    return unless @io
+
+    # kill the subprocess
+    Process.kill "INT", @io.pid
+
+    # close the io
+    @io.close
+    @io = nil
+  end
+
+  def cleanup_runs_directory
+    Dir.glob("#{BASE_DIR}/*")[NUM_RUNS_TO_KEEP..-1]&.sort{|a,b| b <=> a}&.each do |dir|
+      puts "Cleaning up #{dir}"
+      FileUtils.rm_rf(dir)
+    end
+  end
+end
+
+TestRunner.new.run
