2492e26f78807491d2bbbe01df135c06810c822b
Alex Farrill <alex.farrill@gmail.com>
2022-08-05 15:19:24 -0700

    improve cypress tests

--- packages/cli/cypress/e2e/index.cy.js
+++ packages/cli/cypress/e2e/index.cy.js
@@ -1,3 +1,5 @@
+const { isBindingElement } = require("typescript");
+
 describe("cypress tests", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -6,12 +8,25 @@ describe("cypress tests", () => {
   it("should show the preview server index page", () => {
     cy.get("h1").should("contain", "Previews");
     cy.get(".email-group").should("have.length", 2);
-    cy.get(".email-group:first .email-container").should("have.length", 3);
-    cy.get(".email-group:last .email-container").should("have.length", 1);
+
+    // TextEmail group should have 3 previews
+    cy.get(".email-group")
+      .contains("TextEmail")
+      .siblings(".email-container")
+      .should("have.length", 3);
+
+    // Welcome group should have 1 preview
+    cy.get(".email-group")
+      .contains("Welcome")
+      .siblings(".email-container")
+      .should("have.length", 1);
   });
 
   it("clicking a link should load the preview", () => {
-    cy.get(".email-group:first .email-container:first .email").click();
+    cy.get(".email-group .email-container .email")
+      .contains("accountDeleted")
+      .click();
+
     cy.location("pathname").should(
       "match",
       /previews\/TextEmail\.[jt]sx\/accountDeleted/
