c0a767f9fc5e8bfea3bb843041d1a867fd675297
Peter Sugihara <peter@campsh.com>
2022-08-06 10:06:47 -0700

    fix cypress test

--- packages/cli/cypress/e2e/index.cy.js
+++ packages/cli/cypress/e2e/index.cy.js
@@ -4,7 +4,7 @@ describe("cypress tests", () => {
   });
 
   it("should show the preview server index page", () => {
-    cy.get("h1").should("contain", "Previews");
+    cy.get("h1").should("contain", "previews");
     cy.get(".email-group").should("have.length", 2);
     cy.get(".email-group:first .email-container").should("have.length", 3);
     cy.get(".email-group:last .email-container").should("have.length", 1);
