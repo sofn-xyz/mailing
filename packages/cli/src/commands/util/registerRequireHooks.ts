eabfeaf548976821e7f5d93b58eebc0d1b6417b9
Peter Sugihara <peter@campsh.com>
2022-08-13 09:11:17 -0700

    try using swc instead of ts-node and babel

--- packages/cli/src/commands/util/registerRequireHooks.ts
+++ packages/cli/src/commands/util/registerRequireHooks.ts
@@ -1,25 +1,4 @@
 export default function registerRequireHooks() {
   if (process.env.MM_DEV) return;
-
-  require("ts-node").register({
-    compilerOptions: {
-      module: "commonjs",
-      jsx: "react-jsx",
-      moduleResolution: "node",
-      skipLibCheck: true,
-    },
-  });
-
-  require("@babel/register")({
-    presets: [
-      [
-        "@babel/react",
-        {
-          runtime: "automatic",
-        },
-      ],
-      "@babel/preset-env",
-    ],
-    compact: false,
-  });
+  require("@swc/register");
 }
--- packages/cli/src/dev.js
+++ packages/cli/src/dev.js
@@ -3,25 +3,5 @@
 // This script can be used for quick cli development without compilation steps.
 
 process.env.MM_DEV = 1;
-require("ts-node").register({
-  compilerOptions: {
-    module: "commonjs",
-    jsx: "react-jsx",
-    moduleResolution: "node",
-    skipLibCheck: true,
-  },
-});
-
-require("@babel/register")({
-  presets: [
-    [
-      "@babel/react",
-      {
-        runtime: "automatic",
-      },
-    ],
-    "@babel/preset-env",
-  ],
-  compact: false,
-});
+require("@swc/register");
 require("./index.ts");
