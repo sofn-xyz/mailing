3b851c89f301755c6bde9b1b5c5913a8f66e53b0
Peter Sugihara <peter@campsh.com>
2022-08-14 19:50:57 -0700

    remove .swcrc and pass options in code

--- packages/cli/src/commands/util/registerRequireHooks.ts
+++ packages/cli/src/commands/util/registerRequireHooks.ts
@@ -1,4 +1,20 @@
 export default function registerRequireHooks() {
   if (process.env.MM_DEV) return;
-  require("@swc/register");
+  require("@swc/register")({
+    jsc: {
+      parser: {
+        syntax: "typescript",
+        tsx: true,
+        decorators: true,
+      },
+      transform: {
+        react: {
+          runtime: "automatic",
+        },
+      },
+    },
+    module: {
+      type: "commonjs",
+    },
+  });
 }
--- packages/cli/src/dev.js
+++ packages/cli/src/dev.js
@@ -3,5 +3,21 @@
 // This script can be used for quick cli development without compilation steps.
 
 process.env.MM_DEV = 1;
-require("@swc/register");
+require("@swc/register")({
+  jsc: {
+    parser: {
+      syntax: "typescript",
+      tsx: true,
+      decorators: true,
+    },
+    transform: {
+      react: {
+        runtime: "automatic",
+      },
+    },
+  },
+  module: {
+    type: "commonjs",
+  },
+});
 require("./index.ts");
