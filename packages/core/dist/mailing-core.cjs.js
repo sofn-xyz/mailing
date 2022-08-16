8a36b3ac0a59e8e1525326e4db2c9f9178a27bb1
Peter Sugihara <peter@campsh.com>
2022-08-16 09:13:01 -0700

    rm dist

--- packages/core/dist/mailing-core.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./mailing-core.cjs.prod.js");
-} else {
-  module.exports = require("./mailing-core.cjs.dev.js");
-}
