316d222a0864bba58052ae731a591238ad08524e
Alex Farrill <alex.farrill@gmail.com>
2022-08-15 17:02:21 -0700

    fix up tests

--- packages/cli/src/commands/__test__/init.test.ts
+++ packages/cli/src/commands/__test__/init.test.ts
@@ -1,5 +1,5 @@
 import fsExtra, { removeSync, existsSync } from "fs-extra";
-import { handler } from "../init";
+import { handler, InitArguments } from "../init";
 import { log } from "../../log";
 import { ArgumentsCamelCase } from "yargs";
 
@@ -16,58 +16,77 @@ describe("init command", () => {
     removeSync("/tmp/src/emails");
   });
 
-  //   it("creates the ts emails directory", async () => {
-  //     jest
-  //       .spyOn(fsExtra, "existsSync")
-  //       .mockImplementation((path) => /package\.json/.test(path.toString()));
-  //     await handler({} as ArgumentsCamelCase<unknown>);
-  //     expect(log).toHaveBeenCalledWith(
-  //       `Generated your emails dir at /tmp/src/emails:
-  // emails
-  // ├── TextEmail.tsx
-  // ├── Welcome.tsx
-  // ├── components
-  // │   ├── BulletedList.tsx
-  // │   ├── ButtonPrimary.tsx
-  // │   ├── Footer.tsx
-  // │   ├── Head.tsx
-  // │   ├── Header.tsx
-  // │   └── theme.ts
-  // ├── index.ts
-  // └── previews
-  //     ├── TextEmail.tsx
-  //     └── Welcome.tsx`
-  //     );
-  //   });
+  it("creates the ts emails directory", async () => {
+    jest
+      .spyOn(fsExtra, "existsSync")
+      .mockImplementation((path) => /package\.json/.test(path.toString()));
+    await handler({
+      emailsDir: "/tmp/src/emails",
+      typescript: true,
+      port: 3883,
+      quiet: true,
+    } as InitArguments);
+    expect(log).toHaveBeenCalledWith(
+      `Generated your emails dir at /tmp/src/emails:
+emails
+├── TextEmail.tsx
+├── Welcome.tsx
+├── components
+│   ├── BulletedList.tsx
+│   ├── ButtonPrimary.tsx
+│   ├── Footer.tsx
+│   ├── Head.tsx
+│   ├── Header.tsx
+│   └── theme.ts
+├── index.ts
+└── previews
+    ├── TextEmail.tsx
+    └── Welcome.tsx`
+    );
+  });
 
-  //   it("creates the js emails directory", async () => {
-  //     await handler({} as ArgumentsCamelCase<unknown>);
-  //     expect(log).toHaveBeenCalledWith(
-  //       `Generated your emails dir at /tmp/src/emails:
-  // emails
-  // ├── TextEmail.jsx
-  // ├── Welcome.jsx
-  // ├── components
-  // │   ├── BulletedList.jsx
-  // │   ├── ButtonPrimary.jsx
-  // │   ├── Footer.jsx
-  // │   ├── Head.jsx
-  // │   ├── Header.jsx
-  // │   └── theme.js
-  // ├── index.js
-  // └── previews
-  //     ├── TextEmail.jsx
-  //     └── Welcome.jsx`
-  //     );
-  //   });
+  it("creates the js emails directory", async () => {
+    jest
+      .spyOn(fsExtra, "existsSync")
+      .mockImplementation((path) => /package\.json/.test(path.toString()));
 
-  //   it("skips the emails directory if it already exists", async () => {
-  //     const spy = jest
-  //       .spyOn(fsExtra, "existsSync")
-  //       .mockImplementation(() => true);
-  //     expect(fsExtra.existsSync("nothing")).toBe(true);
-  //     await handler({} as ArgumentsCamelCase<unknown>);
-  //     jest.restoreAllMocks();
-  //     expect(fsExtra.existsSync("/tmp/src/emails")).toBe(false);
-  //   });
+    await handler({
+      emailsDir: "/tmp/src/emails",
+      typescript: false,
+      port: 3883,
+      quiet: true,
+    } as InitArguments);
+    expect(log).toHaveBeenCalledWith(
+      `Generated your emails dir at /tmp/src/emails:
+emails
+├── TextEmail.jsx
+├── Welcome.jsx
+├── components
+│   ├── BulletedList.jsx
+│   ├── ButtonPrimary.jsx
+│   ├── Footer.jsx
+│   ├── Head.jsx
+│   ├── Header.jsx
+│   └── theme.js
+├── index.js
+└── previews
+    ├── TextEmail.jsx
+    └── Welcome.jsx`
+    );
+  });
+
+  it("skips the emails directory if it already exists", async () => {
+    const spy = jest
+      .spyOn(fsExtra, "existsSync")
+      .mockImplementation(() => true);
+    expect(fsExtra.existsSync("nothing")).toBe(true);
+    await handler({
+      emailsDir: "./emails",
+      typescript: true,
+      port: 3883,
+      quiet: true,
+    } as InitArguments);
+    jest.restoreAllMocks();
+    expect(fsExtra.existsSync("/tmp/src/emails")).toBe(false);
+  });
 });
