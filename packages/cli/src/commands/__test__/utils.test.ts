9e13e74dec82b9ff1d0ca59fd27a7c261610e4e8
Alex Farrill <alex.farrill@gmail.com>
2022-08-16 11:10:53 -0700

    delete js cruft from npx tsc

---
+++ packages/cli/src/commands/__test__/utils.test.ts
@@ -0,0 +1,6 @@
+import { pick } from "../../utils";
+describe("pick", () => {
+  it("should pick the elements", () => {
+    expect(1).toBe(1);
+  });
+});
--- packages/cli/src/preview/controllers/application.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.renderNotFound = void 0;
-function renderNotFound(res) {
-    res.writeHead(404);
-    res.end("Not found");
-}
-exports.renderNotFound = renderNotFound;
