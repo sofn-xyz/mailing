c8f2bf9f64c2d1c7ca308db5fabb4851eeb76a43
Lubos <lmenus@lmen.us>
2022-08-16 10:28:47 +0100

    fix(hotkeys): remap view hotkeys, add previous mode hotkey

--- packages/cli/src/components/hooks/usePreviewHotkeys.tsx
+++ packages/cli/src/components/hooks/usePreviewHotkeys.tsx
@@ -6,36 +6,51 @@ type Options = {
   setViewMode: React.Dispatch<React.SetStateAction<ViewMode>>;
 };
 
+const hotkeysMap = {
+  showPreviews: '/',
+  viewModeDesktop: '1',
+  viewModeHTML: '3',
+  viewModeMobile: '2',
+  viewModeNext: ']',
+  viewModePrevious: '[',
+};
+
+const viewModeOrder: ViewMode[] = ['desktop', 'mobile', 'html']
+
 // Hotkeys for pages showing previews / intercepts
 // Listen for hotkeys on the document and the iframe's
 // document, so that they still work if iframe has focus.
 export default function usePreviewHotkeys({ setViewMode }: Options) {
   const router = useRouter();
-  const handleKey = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === "/") {
+  const handleKey = useCallback((e: KeyboardEvent) => {
+    switch (e.key) {
+      case hotkeysMap.showPreviews:
         router.push("/");
-      } else if (e.key === "m") {
-        setViewMode("mobile");
-      } else if (e.key === "h") {
-        setViewMode("html");
-      } else if (e.key === "d") {
+        break;
+      case hotkeysMap.viewModeDesktop:
         setViewMode("desktop");
-      } else if (e.key === ".") {
-        setViewMode((current) =>
-          current === "desktop"
-            ? "mobile"
-            : current === "mobile"
-            ? "html"
-            : "desktop"
-        );
-      } else if (e.key === "ArrowRight" || e.key === "right") {
-      } else if (e.key === "ArrowLeft" || e.key === "left") {
-      }
-    },
-    [router, setViewMode]
-  );
-  useHotkeys("m,d,h,.,left,right,/", handleKey);
+        break;
+      case hotkeysMap.viewModeHTML:
+        setViewMode("html");
+        break;
+      case hotkeysMap.viewModeMobile:
+        setViewMode("mobile");
+        break;
+      case hotkeysMap.viewModeNext:
+        setViewMode((viewMode) => {
+          const nextIndex = viewModeOrder.indexOf(viewMode) + 1;
+          return viewModeOrder[nextIndex] || viewModeOrder[0];
+        });
+        break;
+      case hotkeysMap.viewModePrevious:
+        setViewMode((viewMode) => {
+          const nextIndex = viewModeOrder.indexOf(viewMode) - 1;
+          return viewModeOrder[nextIndex] || viewModeOrder[viewModeOrder.length - 1];
+        });
+        break;
+    }
+  }, [router, setViewMode]);
+  useHotkeys(Object.values(hotkeysMap).join(','), handleKey);
 
   const iframeRef = useCallback(
     (node: HTMLIFrameElement) => {
