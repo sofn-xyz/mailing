8349a1743cb3a61c8c53a74fe3a26dfdc15bd567
Peter Sugihara <peter@campsh.com>
2022-08-18 10:15:22 -0700

    add moduleManifest.ts writer

--- packages/cli/src/commands/util/startPreviewServer.ts
+++ packages/cli/src/commands/util/startPreviewServer.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import { relative, resolve } from "path";
 import open from "open";
-import { watch } from "fs-extra";
+import { readdir, watch, writeFile } from "fs-extra";
 import { getPreviewsDirectory } from "../../paths";
 import { error, log, setQuiet } from "../../log";
 import {
@@ -24,11 +24,29 @@ export type PreviewServerOptions = {
   quiet: boolean;
 };
 
+async function writeModuleManifest(previewsPath: string) {
+  const previewCollections = (await readdir(previewsPath)).filter(
+    (path) => !/^\./.test(path)
+  );
+  const uniquePreviewCollections = Array.from(new Set(previewCollections));
+  const previews: string[] = uniquePreviewCollections.map((p) => {
+    return `import ${p.split(".")[0]} from "${resolve(previewsPath, p)};"`;
+  });
+
+  let contents = previews.join("\n") + "\n";
+
+  const manifestPath = resolve(process.cwd(), ".mailing", "moduleManifest.ts");
+  writeFile(manifestPath, contents);
+}
+
 export default async function startPreviewServer(opts: PreviewServerOptions) {
   const { emailsDir, port, quiet } = opts;
   setQuiet(quiet);
 
   registerRequireHooks();
+
+  const previewsPath = getPreviewsDirectory(emailsDir);
+  await writeModuleManifest(previewsPath);
 
   const dev = !!process.env.MM_DEV;
   const hostname = "localhost";
@@ -43,7 +61,6 @@ export default async function startPreviewServer(opts: PreviewServerOptions) {
   const nextHandle = app.getRequestHandler();
   await app.prepare();
 
-  const previewsPath = getPreviewsDirectory(emailsDir);
   if (!previewsPath) {
     error(
       "Could not find emails directory. Have you initialized the project with `mailing init`?"
@@ -55,72 +72,72 @@ export default async function startPreviewServer(opts: PreviewServerOptions) {
   let currentUrl = `${host}/`;
   let shouldReload = false;
 
-  const server = http.createServer(async function (req, res) {
-    const startTime = Date.now();
-    let noLog = false;
+  const server = http
+    .createServer(async function (req, res) {
+      const startTime = Date.now();
+      let noLog = false;
 
-    if (!req.url) {
-      res.end(404);
-      return;
-    }
-
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-
-    // Never cache anything
-    res.setHeader(
-      "Cache-Control",
-      "no-cache, max-age=0, must-revalidate, no-store"
-    );
-    res.setHeader("Pragma", "no-cache");
-    res.setHeader("Expires", "-1");
-
-    currentUrl = `${host}${req.url}`;
-    function showShouldReload(
-      _req: http.IncomingMessage,
-      res: http.ServerResponse
-    ): void {
-      res.writeHead(200);
-      res.end(JSON.stringify({ shouldReload }));
-      shouldReload = false;
-    }
-
-    res.once("close", () => {
-      if (!noLog || process.env.MM_VERBOSE)
-        log(`${res.statusCode} ${req.url} ${Date.now() - startTime}ms`);
-    });
-
-    try {
-      if (req.url === "/previews.json") {
-        // TODO: move this to next app
-        showPreviewIndex(req, res);
-      } else if (req.url === "/should_reload.json") {
-        noLog = true;
-        showShouldReload(req, res);
-      } else if (req.url === "/intercepts" && req.method === "POST") {
-        createIntercept(req, res);
-      } else if (req.url === "/previews/send.json" && req.method === "POST") {
-        await sendPreview(req, res);
-      } else if (/^\/intercepts\/[a-zA-Z0-9]+\.json/.test(req.url)) {
-        showIntercept(req, res);
-      } else if (/^\/previews\/.*\.json/.test(req.url)) {
-        // TODO: move this to next app
-        showPreview(req, res);
-      } else if (/^\/_+next/.test(req.url)) {
-        noLog = true;
-        await app.render(req, res, `${pathname}`, query);
-      } else {
-        // static assets in public directory
-        await nextHandle(req, res, parsedUrl);
+      if (!req.url) {
+        res.end(404);
+        return;
       }
-    } catch (e) {
-      error("caught error", e);
-      res.writeHead(500);
-      res.end(JSON.stringify(e));
-      return;
-    }
-  });
-  server
+
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      // Never cache anything
+      res.setHeader(
+        "Cache-Control",
+        "no-cache, max-age=0, must-revalidate, no-store"
+      );
+      res.setHeader("Pragma", "no-cache");
+      res.setHeader("Expires", "-1");
+
+      currentUrl = `${host}${req.url}`;
+      function showShouldReload(
+        _req: http.IncomingMessage,
+        res: http.ServerResponse
+      ): void {
+        res.writeHead(200);
+        res.end(JSON.stringify({ shouldReload }));
+        shouldReload = false;
+      }
+
+      res.once("close", () => {
+        if (!noLog || process.env.MM_VERBOSE)
+          log(`${res.statusCode} ${req.url} ${Date.now() - startTime}ms`);
+      });
+
+      try {
+        if (req.url === "/previews.json") {
+          // TODO: move this to next app
+          showPreviewIndex(req, res);
+        } else if (req.url === "/should_reload.json") {
+          noLog = true;
+          showShouldReload(req, res);
+        } else if (req.url === "/intercepts" && req.method === "POST") {
+          createIntercept(req, res);
+        } else if (req.url === "/previews/send.json" && req.method === "POST") {
+          await sendPreview(req, res);
+        } else if (/^\/intercepts\/[a-zA-Z0-9]+\.json/.test(req.url)) {
+          showIntercept(req, res);
+        } else if (/^\/previews\/.*\.json/.test(req.url)) {
+          // TODO: move this to next app
+          showPreview(req, res);
+        } else if (/^\/_+next/.test(req.url)) {
+          noLog = true;
+          await app.render(req, res, `${pathname}`, query);
+        } else {
+          // static assets in public directory
+          await nextHandle(req, res, parsedUrl);
+        }
+      } catch (e) {
+        error("caught error", e);
+        res.writeHead(500);
+        res.end(JSON.stringify(e));
+        return;
+      }
+    })
     .listen(port, async () => {
       log(`Running preview at ${currentUrl}`);
       if (!quiet) await open(currentUrl);
