560f7eb483aaeb1588428ccd52b8a2c4b1a8613e
Peter Sugihara <peter@campsh.com>
2022-08-19 22:04:39 -0700

    forgot file

---
+++ packages/cli/src/util/moduleManifestUtil.ts
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+import { previews } from "../moduleManifest";
+
+export function previewTree(): [string, string[]][] {
+  return Object.keys(previews).map((previewName: string) => {
+    const m = previews[previewName as keyof typeof previews];
+    return [previewName, Object.keys(m)];
+  });
+}
+
+export function getPreviewModule(name: string) {
+  return previews[name as keyof typeof previews];
+}
