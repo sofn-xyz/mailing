b4a27e03ae4a22c7b65058a0abc71da736d191f9
Alex Farrill <alex.farrill@gmail.com>
2022-08-22 14:50:48 -0700

    some tests

--- packages/cli/src/commands/util/previewServer/__test__/setup.test.ts
+++ packages/cli/src/commands/util/previewServer/__test__/setup.test.ts
@@ -1,12 +1,48 @@
 import fsExtra from "fs-extra";
-// import * as NodeJS from "node";
 import { packageJsonVersionsMatch } from "../setup";
+import childProcess from "child_process";
+
+jest.mock("../../../../util/log");
 
 describe("packageJsonVersionsMatch", () => {
   it("should return false if .mailing package.json does not exist", async () => {
+    const fileNotFoundError = { code: "ENOENT" };
+
     jest.spyOn(fsExtra, "readFile").mockImplementation((path) => {
-      throw { code: "ENOENT" };
+      throw fileNotFoundError;
     });
+
+    const match = await packageJsonVersionsMatch();
+    expect(match).toBe(false);
+  });
+
+  it("should return true if .mailing package.json matches the cli version", async () => {
+    // mock the .mailing/package.json version
+    jest
+      .spyOn(fsExtra, "readFile")
+      .mockImplementation(
+        (path) =>
+          new Promise((resolve) => resolve(Buffer.from(`{"version":"0.6.6"}`)))
+      );
+
+    // mock the npx mailing version
+    jest.spyOn(childProcess, "execSync").mockImplementation(() => "0.6.6");
+
+    const match = await packageJsonVersionsMatch();
+    expect(match).toBe(true);
+  });
+
+  it("should return false if .mailing package.json does not match the cli version", async () => {
+    // mock the .mailing/package.json version
+    jest
+      .spyOn(fsExtra, "readFile")
+      .mockImplementation(
+        (path) =>
+          new Promise((resolve) => resolve(Buffer.from(`{"version":"1.2.3"}`)))
+      );
+
+    // mock the npx mailing version
+    jest.spyOn(childProcess, "execSync").mockImplementation(() => "4.5.6");
 
     const match = await packageJsonVersionsMatch();
     expect(match).toBe(false);
