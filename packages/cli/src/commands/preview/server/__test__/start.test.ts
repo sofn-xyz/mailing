f0848d67c1f0c531f7dfd7d1a80aad33a2bac33f
Alex Farrill <alex.farrill@gmail.com>
2022-08-23 13:17:53 -0700

    test for start

---
+++ packages/cli/src/commands/preview/server/__test__/start.test.ts
@@ -0,0 +1,27 @@
+import startPreviewServer from "../start";
+import * as config from "../../../../util/config";
+import http from "http";
+import * as paths from "../../../../util/paths";
+
+jest.useFakeTimers();
+
+describe("start", () => {
+  it("should throw an error if it can't find the previews directory", async () => {
+    const mockHttpServer = jest.fn();
+    jest.spyOn(config, "getConfig").mockImplementation(() => {
+      return {
+        emailsDir: "./emails",
+        quiet: true,
+        port: 3883,
+      };
+    });
+
+    jest.spyOn(http, "createServer").mockImplementation(mockHttpServer);
+
+    jest.spyOn(paths, "getPreviewsDirectory").mockImplementation(() => null);
+    await expect(async () => {
+      await startPreviewServer();
+    }).rejects.toThrow("previewsPath is not defined");
+    expect(mockHttpServer).not.toHaveBeenCalled();
+  });
+});
