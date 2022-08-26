cf426899e6ec3366686e3226b31c4ce0c3fbfa2c
Peter Sugihara <peter@campsh.com>
2022-08-25 21:56:17 -0700

    test server build

---
+++ packages/cli/src/commands/__test__/server.test.ts
@@ -0,0 +1,16 @@
+import { execSync } from "child_process";
+
+jest.mock("../../util/log");
+
+describe("server command", () => {
+  it("builds", async () => {});
+
+  describe("e2e", () => {
+    it("builds", () => {
+      const out = execSync(__dirname + "/../../dev.js server build").toString();
+      expect(out).toContain("Compiled successfully");
+      expect(out).toContain("Finalizing page optimization...");
+      expect(out).toContain("First Load JS shared by all");
+    });
+  });
+});
