db41139d49ee75b03c493449f51cd27ba188eb31
Monica Kogler <monica@campsh.com>
2022-08-30 16:31:15 -0700

    more logical and helpful starter templates

---
+++ packages/cli/src/generator_templates/ts/emails/previews/Reservation.tsx
@@ -0,0 +1,71 @@
+import Reservation from "../Reservation";
+import BulletedList from "../components/BulletedList";
+
+export function reservationCanceled() {
+  return (
+    <Reservation
+      headline="Your reservation has been canceled."
+      name="Amelita"
+      bulletedList={
+        <BulletedList
+          items={[
+            "Salazar in Silver Lake",
+            "Sunday, Aug 22 at 1:30pm",
+            "Party of 4, patio",
+          ]}
+        />
+      }
+      body={<>If you have any questions, please reply to this email.</>}
+    />
+  );
+}
+
+export function reservationConfirmed() {
+  return (
+    <Reservation
+      headline="Your reservation is confirmed."
+      name="Amelita"
+      bulletedList={
+        <BulletedList
+          items={[
+            "Salazar in Silver Lake",
+            "Saturday, Aug 22 at 1:30pm",
+            "Party of 4, patio",
+          ]}
+        />
+      }
+      body={
+        <>
+          Thanks for booking your reservation at Salazar with BookBook! If you
+          need to cancel or make any changes, just click the link above.
+        </>
+      }
+      ctaText={"View Reservation"}
+    />
+  );
+}
+
+export function reservationChanged() {
+  return (
+    <Reservation
+      headline="Your reservation has been changed."
+      name="Amelita"
+      bulletedList={
+        <BulletedList
+          items={[
+            "Salazar in Silver Lake",
+            "Sunday, Aug 22 at 1:30pm",
+            "Party of 4, patio",
+          ]}
+        />
+      }
+      body={
+        <>
+          You’re all set! Your reservation at Salazar has been successfully
+          changed. If you have any questions, please reply to this email.
+        </>
+      }
+      ctaText={"View Reservation"}
+    />
+  );
+}
