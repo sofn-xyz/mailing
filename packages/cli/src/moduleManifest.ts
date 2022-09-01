c494c62fcd49c0f16c39d4675ef68ef8c420192b
Peter Sugihara <peter@campsh.com>
2022-08-31 23:13:35 -0700

    update moduleManifest.ts

--- packages/cli/src/moduleManifest.ts
+++ packages/cli/src/moduleManifest.ts
@@ -1,11 +1,17 @@
 import sendMail from "./emails";
-import TextEmail from "./emails/TextEmail";
-import Welcome from "./emails/Welcome";
-import * as TextEmailPreview from "./emails/previews/TextEmail";
-import * as WelcomePreview from "./emails/previews/Welcome";
 
-const previews = { TextEmail: TextEmailPreview, Welcome: WelcomePreview };
-const templates = { TextEmail, Welcome };
+import * as AccountCreatedPreview from "./emails/previews/AccountCreated";
+import * as NewSignInPreview from "./emails/previews/NewSignIn";
+import * as ReservationPreview from "./emails/previews/Reservation";
+import * as ResetPasswordPreview from "./emails/previews/ResetPassword";
+
+const previews = {
+  AccountCreated: AccountCreatedPreview,
+  NewSignIn: NewSignInPreview,
+  Reservation: ReservationPreview,
+  ResetPassword: ResetPasswordPreview,
+};
+const templates = {};
 
 export { templates, previews, sendMail };
 const moduleManifest = { templates, previews, sendMail };
