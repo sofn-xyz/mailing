5a51ccd3bc99fd3c16777cdbde7d9eac31099303
Alex Farrill <alex.farrill@gmail.com>
2022-09-02 16:34:57 -0700

    more tests

--- packages/cli/src/util/__test__/buildHandler.test.ts
+++ packages/cli/src/util/__test__/buildHandler.test.ts
@@ -1,19 +1,75 @@
 import { buildHandler } from "../buildHandler";
+import { log } from "../log";
 import * as postHog from "../postHog";
+import fsExtra from "fs-extra";
+import * as config from "../config";
+
+jest.mock("../log");
 
 describe("buildHandler", () => {
+  beforeEach(() => {
+    // package.json check in buildHandler should pass unless overriden
+    jest.spyOn(fsExtra, "existsSync").mockImplementation(() => true);
+  });
+
   describe("shared guards and setup", () => {
-    it("throws an error if ./package.json is not found", () => {
-      expect(false);
+    it("returns early if ./package.json is not found", async () => {
+      const handler = buildHandler(async () => {}, {});
+      const argv = {
+        anonymousId: "abc",
+        emailsDir: "./emails",
+      };
+
+      jest.spyOn(fsExtra, "existsSync").mockImplementation(() => false);
+
+      const mockWriteDefaultConfigFile = jest.spyOn(
+        config,
+        "writeDefaultConfigFile"
+      );
+
+      await handler(argv);
+
+      expect(log).toHaveBeenCalledWith(
+        "No package.json found. Please run from the project root."
+      );
+
+      // a function that is called a bit later on in the function should not be called :shrug
+      expect(mockWriteDefaultConfigFile).not.toHaveBeenCalled();
     });
-    it("throws an error if emailsDir is not specified in argv", () => {
-      expect(false);
+
+    it("throws an error if emailsDir is not specified in argv", async () => {
+      const handler = buildHandler(async () => {}, {});
+      const argv = {
+        anonymousId: "abc",
+      };
+
+      await expect(async () => {
+        await handler(argv);
+      }).rejects.toThrowError("emailsDir option is not set");
     });
-    it("calls setConfig", () => {
-      expect(false);
-    });
-    it("calls writeDefaultConfigFile", () => {
-      expect(false);
+
+    it("calls setConfig and writeDefaultConfigFile", async () => {
+      const handler = buildHandler(async () => {}, {});
+      const argv = {
+        anonymousId: "abc",
+        emailsDir: "./emails",
+        port: 3883,
+        quiet: true,
+      };
+
+      const mockSetConfig = jest
+        .spyOn(config, "setConfig")
+        .mockImplementation(() => {});
+
+      const mockWriteDefaultConfigFile = jest.spyOn(
+        config,
+        "writeDefaultConfigFile"
+      );
+
+      await handler(argv);
+
+      expect(mockSetConfig).toHaveBeenCalled();
+      expect(mockWriteDefaultConfigFile).toHaveBeenCalled();
     });
   });
 
