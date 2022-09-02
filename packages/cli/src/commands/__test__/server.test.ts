a983aa4e18a5abd3762d27b3cdaf9a025e174c31
Peter Sugihara <peter@campsh.com>
2022-09-02 14:15:02 -0700

    green tests

--- packages/cli/src/commands/__test__/server.test.ts
+++ packages/cli/src/commands/__test__/server.test.ts
@@ -1,10 +1,12 @@
+import { execSync } from "child_process";
 import execCli from "./execCli";
 
 jest.mock("../../util/log");
 
 describe("server command", () => {
   describe("cli", () => {
-    it("builds", () => {
+    it("builds", async () => {
+      execSync("rm -rf .mailing");
       const out = execCli("server build");
       expect(out).toContain("Compiled successfully");
       expect(out).toContain("Finalizing page optimization...");
