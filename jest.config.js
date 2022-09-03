78538fa0ff2eff18d8f97313a4bdf45e5d9d4930
Peter Sugihara <peter@campsh.com>
2022-09-03 16:44:43 -0700

    add cli test, refactor to use async execs for better parallelism

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["scripts"],
   setupFilesAfterEnv: ["<rootDir>/testSetup.ts"],
   watchPathIgnorePatterns: ["tmp-testMailQueue.json"],
+  testTimeout: 20000,
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(js)$": "babel-jest",
+  },
+  transformIgnorePatterns: [],
 };
--- packages/cli/src/commands/__test__/execCli.ts
+++ packages/cli/src/commands/__test__/execCli.ts
@@ -1,7 +1,23 @@
-import { execSync } from "child_process";
+import { exec } from "child_process";
 
-export default function execCli(command: string): string {
-  return execSync(
-    `FORCE_COLOR=0 ${__dirname}/../../dev.js ${command}`
-  ).toString();
+export async function execCli(command: string, opts?: { debug: boolean }) {
+  return new Promise((resolve, reject) => {
+    const child = exec(`FORCE_COLOR=0 ${__dirname}/../../dev.js ${command}`);
+    let out = "";
+    child.stdout?.on("data", (stream) => {
+      out += stream.toString();
+      if (opts?.debug) console.log(out);
+    });
+    child.on("error", reject);
+    child.on("exit", (code) => {
+      if (code) {
+        reject(new Error(`existd with code ${code}`));
+      }
+      resolve(out);
+    });
+  });
+}
+
+export function execCliChild(command: string, opts?: { debug: boolean }) {
+  return exec(`FORCE_COLOR=0 ${__dirname}/../../dev.js ${command}`);
 }
