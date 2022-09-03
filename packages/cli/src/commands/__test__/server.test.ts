73e74f28102aa41f8b4fee7d6970f6b0c5871ad0
Peter Sugihara <peter@campsh.com>
2022-09-03 16:44:43 -0700

    add cli test, refactor to use async execs for better parallelism

--- packages/cli/src/commands/__test__/server.test.ts
+++ packages/cli/src/commands/__test__/server.test.ts
@@ -1,14 +1,30 @@
-import execCli from "./execCli";
+import { execCli, execCliChild } from "./execCli";
+import fetch from "node-fetch";
 
 jest.mock("../../util/log");
 
 describe("server command", () => {
   describe("cli", () => {
-    it("builds", () => {
-      const out = execCli("server build");
+    // n.b. jest runs files sequentially so we just build once at the top
+    it("builds", async () => {
+      const out = await execCli("server build");
       expect(out).toContain("Compiled successfully");
       expect(out).toContain("Finalizing page optimization...");
       expect(out).toContain("First Load JS shared by all");
+    });
+
+    it("starts after building", (done) => {
+      const child = execCliChild("server start");
+      const interval = setInterval(async () => {
+        try {
+          await fetch("http://localhost:3000");
+          clearInterval(interval);
+          child.kill();
+          done();
+        } catch (e) {
+          console.log(e);
+        }
+      }, 1000);
     });
   });
 });
