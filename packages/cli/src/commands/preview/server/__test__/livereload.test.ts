e0741d542b4f7ffefadf942ac4341dd3c9e79934
Peter Sugihara <peter@campsh.com>
2022-09-04 09:50:17 -0700

    more tests

--- packages/cli/src/commands/preview/server/__test__/livereload.test.ts
+++ packages/cli/src/commands/preview/server/__test__/livereload.test.ts
@@ -1,26 +1,37 @@
-import { appendFile, readFile, writeFile } from "fs-extra";
-import fetch from "node-fetch";
+import { readFile, writeFile } from "fs-extra";
+import fetch, { Response } from "node-fetch";
+import type { AbortSignal } from "node-fetch/externals";
 import { WATCH_IGNORE } from "../livereload";
 
 describe("livereload", () => {
   let start: number;
-
-  async function fetchClock(clock: number) {
-    const res = await fetch(
-      `http://localhost:3883/should_reload.json?vectorClock=${clock}`,
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    const json = await res.json();
-    expect(json["vectorClock"]).toBeGreaterThan(0);
-    return json["vectorClock"] as number;
-  }
+  const controller = new AbortController();
+  const signal = controller.signal as AbortSignal;
 
   beforeEach(() => {
     jest.useRealTimers();
     start = Date.now();
   });
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  function constructFetch(clock: number) {
+    return fetch(
+      `http://localhost:3883/should_reload.json?vectorClock=${clock}`,
+      {
+        headers: { "Content-Type": "application/json" },
+        signal,
+      }
+    );
+  }
+
+  async function fetchClock(clock: number) {
+    const res = await constructFetch(clock);
+    const json = await res.json();
+    expect(json["vectorClock"]).toBeGreaterThan(0);
+    return json["vectorClock"] as number;
+  }
 
   it("ignores the right files when watching", () => {
     expect(
@@ -54,6 +65,28 @@ describe("livereload", () => {
     }, 100); // n.b. this must happen after polling
   });
 
-  // it("does not return pollers when a file is touched and vectorClock is ahead", async () => {
-  // });
+  it("does not return poller when a file is touched and vectorClock is ahead", (done) => {
+    let rawFetch: Promise<Response> | null;
+    setTimeout(async () => {
+      try {
+        const startClock = await fetchClock(0);
+        rawFetch = constructFetch(startClock + 1000000);
+        await rawFetch;
+        done.fail("fetch should not return");
+      } catch {
+        // failure is success
+      }
+    });
+
+    setTimeout(async () => {
+      const fileContents = await readFile("./emails/Welcome.tsx");
+      await writeFile("./emails/Welcome.tsx", fileContents);
+    }, 500); // n.b. this must happen after polling
+
+    setTimeout(async () => {
+      // still not returned
+      done();
+      controller.abort();
+    }, 1000);
+  });
 });
