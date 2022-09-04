3d08937063cfb8856b8e80db84d632d7845478b2
Peter Sugihara <peter@campsh.com>
2022-09-03 21:11:42 -0700

    get err

--- packages/cli/src/commands/__test__/execCli.ts
+++ packages/cli/src/commands/__test__/execCli.ts
@@ -4,14 +4,20 @@ export async function execCli(command: string, opts?: { debug: boolean }) {
   return new Promise((resolve, reject) => {
     const child = exec(`FORCE_COLOR=0 ${__dirname}/../../dev.js ${command}`);
     let out = "";
+    let err = "";
     child.stdout?.on("data", (stream) => {
       out += stream.toString();
+      if (opts?.debug) console.log(out);
+    });
+    child.stderr?.on("data", (stream) => {
+      err += stream.toString();
       if (opts?.debug) console.log(out);
     });
     child.on("error", reject);
     child.on("exit", (code) => {
       if (code) {
-        reject(new Error(`existd with code ${code}`));
+        console.error(err);
+        reject(new Error(`${command} exited with code ${code}`));
       }
       resolve(out);
     });
