33a8bd5e0e89054b92777f2b7ea65de6749c19a3
Peter Sugihara <peter@campsh.com>
2022-09-12 15:26:53 -0700

    add test for expand/collapse

--- packages/cli/src/components/IndexPane/CompactView.tsx
+++ packages/cli/src/components/IndexPane/CompactView.tsx
@@ -31,38 +31,44 @@ const CompactView: React.FC<CompactViewProps> = ({
               role="treeitem"
               aria-expanded={!route.collapsed}
               aria-selected={i === cursor}
-              className={cx("pb-1 pt-1 mb-1", {
+              className={cx("mb-1", {
                 "pl-2": route.level === 0,
                 "pl-6": route.level === 1,
                 "pl-12": route.level === 2,
                 "bg-blue text-black rounded-2xl": i === cursor,
+                "pb-1 pt-1": route.level === 2,
               })}
               onClick={() => navigate(i)}
             >
               {route.level < 2 && (
-                <svg
-                  width="9"
-                  height="9"
-                  viewBox="0 0 9 9"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={cx(
-                    "inline origin-center arrow transition-transform mr-3",
-                    {
-                      "stroke-black": i === cursor,
-                      "stroke-white": i !== cursor,
-                      "collapsed -rotate-90 relative -top-[1px]":
-                        route.collapsed,
-                    }
-                  )}
+                <button
+                  className="py-1 px-3"
                   onClick={() => setCollapse(i, !route.collapsed)}
+                  aria-label={`Toggle ${route.path}`}
                 >
-                  <path
-                    d="M1.5 1.5L4.5 4.5L7.5 1.5"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                  />
-                </svg>
+                  <svg
+                    width="9"
+                    height="9"
+                    viewBox="0 0 9 9"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={cx(
+                      "inline origin-center arrow transition-transform",
+                      {
+                        "stroke-black": i === cursor,
+                        "stroke-white": i !== cursor,
+                        "collapsed -rotate-90 relative -top-[1px]":
+                          route.collapsed,
+                      }
+                    )}
+                  >
+                    <path
+                      d="M1.5 1.5L4.5 4.5L7.5 1.5"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                    />
+                  </svg>
+                </button>
               )}
               {route.displayName}
             </div>
--- packages/cli/src/components/IndexPane/__test__/IndexPane.test.tsx
+++ packages/cli/src/components/IndexPane/__test__/IndexPane.test.tsx
@@ -58,17 +58,39 @@ describe("IndexPane", () => {
       expect(selected.textContent).toBe("Emails");
     });
 
-    // it("selects an item with a click", async () => {
-    //   const { findByRole, user, findByText } = setup(<IndexPane previews={previews} />);
-    //   const tree = await findByRole("tree");
-    //   const item = await findByText("");
-    //   await user.click(item)
-    //   const selected = await findByRole("treeitem", {
-    //     selected: true,
-    //     expanded: false,
-    //   });
-    //   expect(selected.textContent).toBe("Emails");
-    // });
+    it("selects an item with a click", async () => {
+      const { findByRole, user, findByText } = setup(
+        <IndexPane previews={previews} />
+      );
+      const item = await findByText("AccountCreated");
+      await user.click(item);
+      const selected = await findByRole("treeitem", {
+        selected: true,
+        expanded: false,
+      });
+      expect(selected.textContent).toBe("AccountCreated");
+    });
+
+    it("collapses an item with a click", async () => {
+      const { findByRole, user, findByLabelText, findByText } = setup(
+        <IndexPane previews={previews} />
+      );
+      const item = await findByLabelText("Toggle /previews/AccountCreated");
+      await user.click(item);
+      const collapsed = await findByRole("treeitem", {
+        selected: true,
+        expanded: false,
+      });
+      expect(collapsed.textContent).toBe("AccountCreated");
+
+      await user.click(item);
+
+      const expanded = await findByRole("treeitem", {
+        selected: true,
+        expanded: true,
+      });
+      expect(expanded.textContent).toBe("AccountCreated");
+    });
   });
 
   describe("ClientView", () => {
