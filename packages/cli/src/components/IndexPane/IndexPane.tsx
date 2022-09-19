054676d3ffe5167866048a5be41af55afbedf2f8
Peter Sugihara <peter@campsh.com>
2022-09-19 11:10:23 -0700

    remove null state for now

--- packages/cli/src/components/IndexPane/IndexPane.tsx
+++ packages/cli/src/components/IndexPane/IndexPane.tsx
@@ -41,14 +41,6 @@ const IndexPane: React.FC<IndexPaneProps> = ({ previews, previewText }) => {
     setCompact((current) => !current);
   };
 
-  const showNullState =
-    !previews ||
-    previews.length === 0 ||
-    (previews.length === 2 &&
-      previews[0][0] === "TextEmail" &&
-      previews[1][0] === "Welcome" &&
-      !process.env.NEXT_PUBLIC_STATIC);
-
   const view = compact ? (
     <CompactView
       treeRoutes={treeRoutes}
@@ -91,13 +83,7 @@ const IndexPane: React.FC<IndexPaneProps> = ({ previews, previewText }) => {
           </div>
         </label>
       </div>
-      {showNullState && (
-        <div className="null-sub">
-          Build new email templates in <span className="code">emails</span>. Add
-          previews to <span className="code">emails/previews</span> and they’ll
-          appear below.
-        </div>
-      )}
+
       {previews?.map ? view : "Loading"}
       <style jsx>{`
         #toggle-compact-view .slider:before {
