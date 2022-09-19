f7f65b58f7b3134c4b0e8e5ddfc52a83e424450f
Monica Kogler <monica@campsh.com>
2022-09-18 20:39:30 -0700

    rework it

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -6,9 +6,9 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <>
-      <div className={`${styles.pageContainer} w-full`}>
+      <div className="w-full">
         <Head>
-          <title>Mailing – Build + test + send emails with React</title>
+          <title>Mailing – Build emails in React, send from anywhere</title>
           <meta property="og:title" content="Mailing" />
           <meta
             name="description"
@@ -35,58 +35,54 @@ const Home: NextPage = () => {
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div
-          className={`${styles.bodyContainer} flex justify-start sm:justify-center pt-16 pb-44`}
-        >
-          <div className="px-6 sm:mx-8">
-            <header className="flex justify-between items-top pb-28">
-              <div className="brand">
-                <span className="hidden sm:block">
-                  <Image
-                    width="146"
-                    height="32"
-                    src="/mailing-logo.svg"
-                    alt="Mailing logo"
-                  />
-                </span>
-                <span className="sm:hidden">
-                  <Image
-                    width="25.6"
-                    height="32"
-                    src="/mailing-icon.svg"
-                    alt="Mailing icon"
-                  />
-                </span>
-              </div>
-              <nav>
+        <main>
+          <div className={`${styles.heroContainer} px-6 md:px-12 pt-16 pb-44`}>
+            <div className="max-w-screen-xl mx-auto">
+              <div className="flex justify-between items-top pb-28">
+                <div className="brand">
+                  <span className="hidden sm:block">
+                    <Image
+                      width="146"
+                      height="32"
+                      src="/mailing-logo.svg"
+                      alt="Mailing logo"
+                    />
+                  </span>
+                  <span className="sm:hidden">
+                    <Image
+                      width="25.6"
+                      height="32"
+                      src="/mailing-icon.svg"
+                      alt="Mailing icon"
+                    />
+                  </span>
+                </div>
                 <a
                   className="hover:bg-yellow text-base leading-none inline-block"
                   href="https://demo.mailing.run"
                 >
                   Demo
                 </a>
-              </nav>
-            </header>
-            <main className={styles.main}>
-              <div className="grid grid-flow-col auto-cols-max">
-                <div className="max-w-[440px]">
-                  <h1 className="text-4xl sm:text-6xl pt-3">
+              </div>
+              <div className="flex flex-col lg:flex-row justify-between md:items-center">
+                <div className="lg:max-w-[540px] text-left md:text-center lg:text-left">
+                  <h1 className="text-3xl md:text-5xl lg:text-6xl md:max-w-md text-left md:text-center lg:text-left md:mx-auto">
                     Build emails in React, send from anywhere
                   </h1>
                   <p
-                    className="text-2xl leading-snug m-0 pt-4 pb-8 max-w-sm"
+                    className="text-2xl leading-snug m-0 pt-4 pb-8 lg:max-w-sm"
                     id={styles.sub}
                   >
                     An open source email development and analytics tool
                   </p>
                   <a
-                    className="bg-blue-400 text-black text-xl leading-none px-8 pt-5 pb-6 block rounded-2xl hover:underline absolute sm:static w-100 sm:w-fit right-[24px] bottom-[32px] left-[24px] text-center"
+                    className="bg-blue-400 text-black text-xl inline-block leading-none px-8 pt-5 pb-6 rounded-2xl hover:underline mx-auto"
                     href="https://github.com/sofn-xyz/mailing"
                   >
                     Get Started on GitHub
                   </a>
                 </div>
-                <div className="pl-36">
+                <div className="md:pl-0 mt-10 lg:mt-0 hidden sm:block mx-auto lg:ml-6 lg:mr-0">
                   <Image
                     className="rounded-2xl"
                     width="640"
@@ -96,10 +92,9 @@ const Home: NextPage = () => {
                   />
                 </div>
               </div>
-            </main>
-            <footer></footer>
+            </div>
           </div>
-        </div>
+        </main>
       </div>
     </>
   );
