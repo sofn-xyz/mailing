526b0a7c759264c4c53a8d004bd923b5332a6af5
Peter Sugihara <peter@campsh.com>
2022-09-26 12:28:57 -0700

    comment

--- packages/cli/src/commands/__test__/deploy.test.ts
+++ packages/cli/src/commands/__test__/deploy.test.ts
@@ -4,11 +4,10 @@ import { handler, InitArguments } from "../init";
 import { log } from "../../util/log";
 import { execCli } from "./execCli";
 
-// jest.useFakeTimers();
 jest.mock("../../util/log");
 
 describe("init command", () => {
-  it("runs deploy", async () => {
+  it("fails without vercel login", async () => {
     await expect(async () => {
       await execCli("deploy");
     }).rejects.toThrowError(
