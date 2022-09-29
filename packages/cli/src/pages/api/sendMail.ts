e4730099fba564cc4ac8714a6e006480c9393c4d
Alex Farrill <alex.farrill@gmail.com>
2022-09-29 15:47:24 -0700

    sendMail api

--- packages/cli/src/pages/api/sendMail.ts
+++ packages/cli/src/pages/api/sendMail.ts
@@ -1,6 +1,7 @@
 import React from "react";
+import { createTransport } from "nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { render } from "mailing-core";
+import buildSendMail, { render } from "mailing-core";
 import { MjmlError } from "mjml-react";
 import { templates } from "../../moduleManifest";
 
@@ -27,6 +28,8 @@ function renderTemplate(
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") return res.status(404).end();
+
   const { templateName, props } = req.query;
 
   // validate template name
@@ -54,5 +57,28 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (error) {
     return res.status(404).json({ error } as Data);
   }
-  res.status(200).json({ html, mjmlErrors } as Data);
+
+  const transport = createTransport({
+    pool: true,
+    host: "email-smtp.us-east-1.amazonaws.com",
+    port: 465,
+    secure: true, // use TLS
+    auth: {
+      user: process.env.SES_USER,
+      pass: process.env.SES_PASSWORD,
+    },
+  });
+
+  const options = {
+    transport,
+    defaultFrom: "alex.farrill@gmail.com",
+    configPath: "",
+  };
+
+  const sendMail = buildSendMail(options);
+  const to = "alex.farrill@gmail.com";
+
+  sendMail({ to, html });
+
+  res.status(200).end();
 }
