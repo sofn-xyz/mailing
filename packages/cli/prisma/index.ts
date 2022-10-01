f1e3b84641ddc070f686febb30c0324626e4d7a9
Peter Sugihara <peter@campsh.com>
2022-09-30 21:10:22 -0700

    global prisma

--- packages/cli/prisma/index.ts
+++ packages/cli/prisma/index.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+declare global {
+  var prisma: PrismaClient<any>;
+}
+
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
