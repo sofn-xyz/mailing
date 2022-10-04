51b7b4da2e7f93d605f4ef026da2c6e6e811d06a
Alex Farrill <alex.farrill@gmail.com>
2022-10-04 14:53:27 -0700

    nodemail opts

--- packages/cli/src/pages/api/sendMail.ts
+++ packages/cli/src/pages/api/sendMail.ts
@@ -1,9 +1,8 @@
 import React from "react";
-import { createTransport } from "nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
-import buildSendMail, { render } from "mailing-core";
+import { render } from "mailing-core";
 import { MjmlError } from "mjml-react";
-import { templates } from "../../moduleManifest";
+import { templates, sendMail } from "../../moduleManifest";
 
 type Data = {
   error?: string; // api error messages
@@ -42,13 +41,37 @@ export default async function handler(
 ) {
   if (req.method !== "POST") return res.status(404).end();
 
+  const {
+    templateName,
+    props,
+    to,
+    subject,
+    cc,
+    bcc,
+    from,
+    text,
+    sender,
+    replyTo,
+    inReplyTo,
+    references,
+  } = req.body;
+
   if (!(await validApiKey(req.headers["X-API-Key"])))
     return res.status(401).json({
       error:
         "please use a valid API key or create one at https://www.mailing.run/settings",
     });
 
-  const { templateName, props } = req.query;
+  // validate to, cc, bcc name
+  if (
+    typeof to !== "string" &&
+    typeof cc !== "string" &&
+    typeof bcc !== "string"
+  ) {
+    return res
+      .status(403)
+      .json({ error: "to, cc, or bcc must be specified" } as Data);
+  }
 
   // validate template name
   if (typeof templateName !== "string") {
@@ -76,27 +99,19 @@ export default async function handler(
     return res.status(404).json({ error } as Data);
   }
 
-  const transport = createTransport({
-    pool: true,
-    host: "email-smtp.us-east-1.amazonaws.com",
-    port: 465,
-    secure: true, // use TLS
-    auth: {
-      user: process.env.SES_USER,
-      pass: process.env.SES_PASSWORD,
-    },
+  sendMail({
+    html,
+    to,
+    cc,
+    bcc,
+    subject,
+    from,
+    text,
+    sender,
+    replyTo,
+    inReplyTo,
+    references,
   });
-
-  const options = {
-    transport,
-    defaultFrom: "alex.farrill@gmail.com",
-    configPath: "",
-  };
-
-  const sendMail = buildSendMail(options);
-  const to = "alex.farrill@gmail.com";
-
-  sendMail({ to, html });
 
   res.status(200).end();
 }
