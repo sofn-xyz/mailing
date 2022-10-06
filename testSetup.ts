d1b1a0c8ffd5d0247cf4469caec63776a7362327
Peter Sugihara <peter@campsh.com>
2022-10-06 14:58:42 -0700

    database cleaning

--- testSetup.ts
+++ testSetup.ts
@@ -1,3 +1,33 @@
+import { execSync } from "child_process";
+
+const WEB_TABLE_NAMES = [
+  "NewsletterSubscriber",
+  "Organization",
+  "User",
+  "ApiKey",
+  "OauthAccessToken",
+  "OauthAuthorizationCode",
+];
+
+const TABLE_NAMES = ["Organization"];
+
+beforeAll(() => {
+  const truncateSql = TABLE_NAMES.map(
+    (tableName) => `TRUNCATE TABLE "${tableName}" CASCADE;`
+  ).join(" ");
+  execSync(`echo '${truncateSql}' | psql ${process.env.DATABASE_URL_TEST}`, {
+    stdio: "ignore",
+  });
+
+  const webTruncateSql = WEB_TABLE_NAMES.map(
+    (tableName) => `TRUNCATE TABLE "${tableName}" CASCADE;`
+  ).join(" ");
+  execSync(
+    `echo '${webTruncateSql}' | psql ${process.env.WEB_DATABASE_URL_TEST}`,
+    { stdio: "ignore" }
+  );
+});
+
 afterEach(() => {
   jest.resetAllMocks();
 });
