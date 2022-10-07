a7dd2173e0de85abb32f8c2614883ed0f089be87
Elliot Hursh <elliothursh@gmail.com>
2022-10-07 12:23:59 -0700

    share tailwind theme

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,31 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+const baseConfig = require("../../tailwind.config.cjs");
+
 module.exports = {
+  ...{ baseConfig },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        white: "#E4EBFA",
-        black: "#111",
-        blue: "#B8CEFF",
-        yellow: "#fafa98",
-        blue: {
-          300: "#dce7ff",
-          400: "#a1bfff",
-        },
-        green: {
-          50: "#f1fcef",
-        },
-        neutral: {
-          800: "#222",
-        },
-      },
-      fontSize: {
-        "6xl": "64px",
-      },
-    },
-  },
-  plugins: [],
 };
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        white: "#E4EBFA",
+        black: "#111",
+        blue: "#B8CEFF",
+        yellow: "#fafa98",
+        blue: {
+          300: "#dce7ff",
+          400: "#a1bfff",
+        },
+        green: {
+          50: "#f1fcef",
+        },
+        neutral: {
+          800: "#222",
+        },
+      },
+      fontSize: {
+        "6xl": "64px",
+      },
+    },
+  },
+  plugins: [],
+};
