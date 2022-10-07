a1ba76d0b5a78a024238c8c6b11c78e890d54914
Alex Farrill <alex.farrill@gmail.com>
2022-10-07 15:00:57 -0700

    comment

--- testSetup.integration.ts
+++ testSetup.integration.ts
@@ -7,7 +7,7 @@ import chalk from "chalk";
 if (!process.env.MAILING_CI) {
   throw new Error(
     chalk.red(
-      "Refusing to run outside of CI mode! WARNING: running the e2e tests against your development server will cause test data to be inserted into your development database.  To run these tests locally, use `yarn ci:e2e` instead."
+      "Refusing to run outside of CI mode! WARNING: running the integration tests against your development server will cause test data to be inserted into your development database.  To run these tests locally, use `yarn ci:integration` instead."
     )
   );
 }
