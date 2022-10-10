0bcf322bdf16065c45d84a81e8d88c8497e170a5
Alex Farrill <alex.farrill@gmail.com>
2022-10-10 11:24:47 -0700

    - remove cli and web prisma migrations - update cli and web schemas - i didn't run prisma migrate dev yet

--- packages/cli/prisma/migrations/20220930230357_add_organization/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Organization" (
-    "id" TEXT NOT NULL,
-    "clientSecret" TEXT NOT NULL,
-
-    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
-);
--- packages/web/prisma/migrations/20220928212526_sync/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "User_organizationId_idx" ON "User"("organizationId");
