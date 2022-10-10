7fd09ae6fdd3844c01fbc90c90af9438c1827cb9
Peter Sugihara <peter@campsh.com>
2022-10-10 13:07:16 -0700

    move title from _document to _app (#247)

--- packages/cli/src/pages/_app.tsx
+++ packages/cli/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { HamburgerProvider } from "../components/HamburgerContext";
 import "../styles/globals.css";
 
 export default function Mailing({ Component, pageProps }: AppProps) {
   return (
     <HamburgerProvider>
+      <Head>
+        <title>Mailing</title>
+      </Head>
       <Component {...pageProps} />;
     </HamburgerProvider>
   );
