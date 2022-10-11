3bde99006ee14c53c198fa761b39eb9b19a1b919
Alex Farrill <alex.farrill@gmail.com>
2022-10-11 12:52:31 -0700

    text

--- packages/cli/src/pages/login.tsx
+++ packages/cli/src/pages/login.tsx
@@ -80,7 +80,7 @@ const Login: NextPage = () => {
                 </div>
                 <div className="mt-[12px]">
                   <button className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    Save
+                    Login
                   </button>
                 </div>
               </form>
