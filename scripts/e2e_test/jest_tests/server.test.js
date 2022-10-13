c5ef03ba426d4cee7f89f7486bc1462304c8eaa9
Elliot Hursh <elliothursh@gmail.com>
2022-10-12 20:13:27 -0700

    try just building

--- scripts/e2e_test/jest_tests/server.test.js
+++
@@ -1,8 +0,0 @@
-import execCli from "./util/execCli";
-
-describe("server command", () => {
-  it("runs build", async () => {
-    // expect it not to raise an error
-    await execCli("server build");
-  });
-});
