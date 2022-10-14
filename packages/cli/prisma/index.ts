b942926b0f8a9a68d36008a9755b01c3e83a5bce
Peter Sugihara <peter@campsh.com>
2022-10-14 13:03:28 -0700

    fix cliPrisma types, delete with prisma... 100ms -> 30ms

--- packages/cli/prisma/index.ts
+++ packages/cli/prisma/index.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "./generated/client";
 
 declare global {
-  const prisma: PrismaClient<any>;
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient<any>;
 }
 
-const prisma = (global as any).prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "development") (global as any).prisma = prisma;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
--- testUtilIntegration.ts
+++ testUtilIntegration.ts
@@ -1,7 +1,6 @@
-import { execSync } from "child_process";
 import chalk from "chalk";
 import cliPrisma from "./packages/cli/prisma";
-import cliWeb from "./packages/web/prisma";
+import webPrisma from "./packages/web/prisma";
 
 // logging
 const { DEBUG } = process.env;
@@ -17,77 +16,16 @@ export function debug(message?: any, ...optionalParams: any[]) {
     console.info(chalk.yellowBright(PREFIX), message, ...optionalParams);
 }
 
-// database cleaning
-const WEB_TABLE_NAMES = ["NewsletterSubscriber"];
-
-const CLI_TABLE_NAMES = ["ApiKey", "Organization", "User"];
-
 export async function truncateCliDatabase() {
-  const truncateSql = CLI_TABLE_NAMES.map(
-    (tableName) => `TRUNCATE TABLE "${tableName}" CASCADE;`
-  ).join(" ");
   debug("Running TRUNCATE on ", process.env.DATABASE_URL_TEST);
-  execSync(`echo '${truncateSql}' | psql ${process.env.DATABASE_URL_TEST}`, {
-    stdio: "ignore",
-  });
 
-  await waitForEmptyCliDatabase();
-}
-
-/* waitForEmptyCliDatabase
-
-Although TRUNCATE has been run, the database often has 
-not finished the process of truncating the data when 
-the tests run, which leads to flaky tests.
-
-*/
-async function waitForEmptyCliDatabase() {
-  let recordsCount = 1;
-  let iterations = 0;
-
-  while (recordsCount > 0) {
-    recordsCount = ((await cliPrisma.user.count()) +
-      (await cliPrisma.apiKey.count()) +
-      (await cliPrisma.organization.count())) as number;
-
-    if (9 === iterations % 10)
-      console.log(`Waiting for cli database tables to empty`);
-
-    iterations += 1;
-  }
+  await cliPrisma.apiKey.deleteMany({});
+  await cliPrisma.user.deleteMany({});
+  await cliPrisma.organization.deleteMany({});
 }
 
 export async function truncateWebDatabase() {
-  const webTruncateSql = WEB_TABLE_NAMES.map(
-    (tableName) => `TRUNCATE TABLE "${tableName}" CASCADE;`
-  ).join(" ");
   debug("Running TRUNCATE on ", process.env.WEB_DATABASE_URL_TEST);
-  execSync(
-    `echo '${webTruncateSql}' | psql ${process.env.WEB_DATABASE_URL_TEST}`,
-    { stdio: "ignore" }
-  );
 
-  await waitForEmptyWebDatabase();
-}
-
-/* waitForEmptyWebDatabase
-
-Although TRUNCATE has been run, the database often has 
-not finished the process of truncating the data when 
-the tests run, which leads to flaky tests.
-
-*/
-
-async function waitForEmptyWebDatabase() {
-  let recordsCount = 1;
-  let iterations = 0;
-
-  while (recordsCount > 0) {
-    recordsCount = await cliWeb.newsletterSubscriber.count();
-
-    if (9 === iterations % 10)
-      console.log(`Waiting for cli database tables to empty`);
-
-    iterations += 1;
-  }
+  await webPrisma.newsletterSubscriber.deleteMany({});
 }
