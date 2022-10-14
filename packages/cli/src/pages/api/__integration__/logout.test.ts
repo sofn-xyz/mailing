b60ed084d38149936a46c6dcaf58374c09fc7ac5
Peter Sugihara <peter@campsh.com>
2022-10-14 13:32:28 -0700

    logout integration test

---
+++ packages/cli/src/pages/api/__integration__/logout.test.ts
@@ -0,0 +1,28 @@
+import { apiFetch, apiLogin } from "../apiIntegrationTestUtil";
+
+describe("logout", () => {
+  describe("logged out", () => {
+    it("does nothing", async () => {
+      const apiKeysResLoggedOut = await apiFetch("/api/apiKeys");
+      expect(apiKeysResLoggedOut.status).toBe(404);
+
+      const logoutRes = await apiFetch("/api/logout");
+      expect(logoutRes.status).toBe(200);
+    });
+  });
+
+  describe("logged in", () => {
+    it("logs out", async () => {
+      await apiLogin();
+
+      const apiKeysRes = await apiFetch("/api/apiKeys");
+      expect(apiKeysRes.status).toBe(200);
+
+      const logoutRes = await apiFetch("/api/logout");
+      expect(logoutRes.status).toBe(200);
+
+      const apiKeysResLoggedOut = await apiFetch("/api/apiKeys");
+      expect(apiKeysResLoggedOut.status).toBe(404);
+    });
+  });
+});
