19ac2f67785425116e4e8d37c52afb0cddf4e9f0
Elliot Hursh <elliothursh@gmail.com>
2022-10-17 13:48:10 -0700

    try this

--- e2e/app/redwood_js.rb
+++ e2e/app/redwood_js.rb
@@ -18,5 +18,17 @@ module App
         system_quiet('yarn add next react react-dom')
       end
     end
+
+    def add_yarn_ci_scripts!
+      super
+
+      Dir.chdir(root_dir) do
+        package_json = JSON.parse(File.read('package.json'))
+        package_json['resolutions'] ||= {}
+        package_json['resolutions']['@types/react'] = '^17'
+        package_json['resolutions']['@types/react-dom'] = '^17'
+        File.write('package.json', JSON.pretty_generate(package_json))
+      end
+    end
   end
 end
