6d0e5f7eb940f27210d235c399ce7d0979c19cdf
Peter Sugihara <peter@campsh.com>
2022-10-18 09:37:32 -0700

    disconnect prisma clients on each run, truncate dbs concurrently

--- packages/cli/prisma/index.ts
+++ packages/cli/prisma/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "./generated/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient<any>;
+  var prisma: PrismaClient<any> | undefined;
 }
 
 const prisma = global.prisma || new PrismaClient();
