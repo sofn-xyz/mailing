2cc18ad661a1db3f55f539375e65bb3a93346ca2
Alex Farrill <alex.farrill@gmail.com>
2022-10-27 11:49:25 -0700

    add cy test that duplicates the bug

--- packages/cli/cypress/e2e/__integration__/login.cy.integration.js
+++ packages/cli/cypress/e2e/__integration__/login.cy.integration.js
@@ -4,7 +4,25 @@ describe("login tests", () => {
   });
 
   it("should show the login page", () => {
-    cy.visit("/login");
+    cy.visit("/signup");
+    cy.location("pathname").should("eq", "/signup");
+
+    cy.get("h1").should("contain", "Signup");
+    cy.get("input#email").should("exist");
+
+    // fill in email and passord fields and then submit the form
+    cy.get("input#email").type("test@test.com");
+    cy.get("input#password").type("password");
+    cy.get("form").submit();
+
+    // it should redirect to the login page
     cy.location("pathname").should("eq", "/login");
+
+    // fill in email and passord fields and then submit the form
+    cy.get("input#email").type("test@test.com");
+    cy.get("input#password").type("password");
+    cy.get("form").submit();
+
+    cy.location("pathname").should("eq", "/settings");
   });
 });
