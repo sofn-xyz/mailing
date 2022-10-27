816619290b8f23294acc70d49ec6e7d506df0893
Alex Farrill <alex.farrill@gmail.com>
2022-10-26 18:40:00 -0700

    small refactor for readability

--- packages/cli/cypressIntegration.config.ts
+++ packages/cli/cypressIntegration.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "cypress";
 import cliPrisma from "./prisma";
 
+const resetDb = async () => {
+  const count = await cliPrisma.$queryRaw`SELECT COUNT(*) FROM "User";`;
+  console.log("count is", count);
+  return null;
+};
+
 export default defineConfig({
   e2e: {
     specPattern: "**/*.cy.integration.{js,jsx,ts,tsx}",
@@ -8,11 +14,7 @@ export default defineConfig({
     baseUrl: "http://localhost:3883",
     setupNodeEvents(on, _config) {
       on("task", {
-        async "db:reset"() {
-          const count = await cliPrisma.$queryRaw`SELECT COUNT(*) FROM "User";`;
-          console.log("count is", count);
-          return null;
-        },
+        "db:reset": resetDb,
       });
     },
   },
