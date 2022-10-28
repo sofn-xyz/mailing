192f2e0a90467770c3a969ccff0d248a90c23f81
Peter Sugihara <peter@campsh.com>
2022-10-27 23:13:41 -0700

    revert template change

--- packages/cli/src/generator_templates/ts/emails/AccountCreated.tsx
+++ packages/cli/src/generator_templates/ts/emails/AccountCreated.tsx
@@ -29,7 +29,7 @@ const AccountCreated: React.FC<{ name: string }> = ({ name }) => (
             cssClass="hero"
             padding="0 0 40px"
             align="left"
-            src="/lab.campsh.com/bb-hero%402x.jpg"
+            src="https://s3.amazonaws.com/lab.campsh.com/bb-hero%402x.jpg"
           />
         </MjmlColumn>
       </MjmlSection>
