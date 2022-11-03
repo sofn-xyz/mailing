7f5eb4add213a2f90c2c8fcfc890bc8b79bb640c
Peter Sugihara <peter@campsh.com>
2022-11-03 14:03:15 -0700

    swc/register to esbuild (#309)
    * use esbuild rather than swc for dynamic ts imports
    
    * jsx: automatic
    
    * specify target node

--- packages/cli/src/commands/util/registerRequireHooks.ts
+++ packages/cli/src/commands/util/registerRequireHooks.ts
@@ -1,21 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 export default function registerRequireHooks() {
   if (process.env.MM_DEV) return;
-  require("@swc/register")({
-    jsc: {
-      parser: {
-        syntax: "typescript",
-        tsx: true,
-        decorators: true,
-      },
-      transform: {
-        react: {
-          runtime: "automatic",
-        },
-      },
-    },
-    module: {
-      type: "commonjs",
-    },
+  require("esbuild-register/dist/node").register({
+    jsx: "automatic",
+    target: "node14",
   });
 }
