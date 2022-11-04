f2d1899d3350385aa8ce43bc07102a63b8a18886
Alex Farrill <alex.farrill@gmail.com>
2022-11-04 11:03:17 -0700

    log the error

--- packages/cli/src/pages/api/lists/[id]/subscribe.ts
+++ packages/cli/src/pages/api/lists/[id]/subscribe.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next/types";
 import prisma from "../../../../../prisma";
 import type { Prisma } from "../../../../../prisma/generated/client";
+import { error } from "../../../../util/log";
 
 type Data = {
   error?: string;
@@ -26,14 +27,15 @@ const ApiSubscribe = async function (
         });
 
         return res.status(201).end();
-      } catch (error) {
+      } catch (e) {
         // having some trouble with the above, so just assuming it's a duplicate for now
         // the best practice code descired in the prisma docs doesn't work because typeof error is not Prisma.PrismaClientKnownRequestError
-        if ((error as Prisma.PrismaClientKnownRequestError).code === "P2002") {
+        if ((e as Prisma.PrismaClientKnownRequestError).code === "P2002") {
           return res
             .status(422)
             .json({ error: "You're already subscribed to that list" });
         } else {
+          error("an unknown error occurred", e);
           return res.status(500).json({ error: "an unknown error occurred" });
         }
       }
