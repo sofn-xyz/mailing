f4b7185797dff9947ad874f9843373a52125dcbc
Peter Sugihara <peter@campsh.com>
2022-11-10 19:38:14 -0800

    golf i couldn't stop thinking about

--- packages/cli/src/util/lists.ts
+++ packages/cli/src/util/lists.ts
@@ -3,8 +3,6 @@ import type { Organization } from "../../prisma/generated/client";
 
 // find or create the default list
 export async function findOrCreateDefaultList(org?: Organization | null) {
-  let organization = org;
-
   let defaultList = await prisma.list.findFirst({
     where: {
       isDefault: true,
@@ -12,12 +10,8 @@ export async function findOrCreateDefaultList(org?: Organization | null) {
   });
 
   if (!defaultList) {
-    if (!organization) {
-      organization = await prisma.organization.findFirst({});
-    }
-    if (!organization) {
-      throw new Error("No organization found");
-    }
+    const organization = org || (await prisma.organization.findFirst());
+    if (!organization) throw new Error("No organization found");
 
     defaultList = await prisma.list.create({
       data: {
