234dcf262b9c833837b6ee96178f3e45651a268c
Alex Farrill <alex.farrill@gmail.com>
2022-11-14 14:05:57 -0800

    sendmail tests

--- packages/cli/src/pages/api/__integration__/sendMail.test.ts
+++ packages/cli/src/pages/api/__integration__/sendMail.test.ts
@@ -5,6 +5,7 @@ import prisma from "../../../../prisma";
 import { apiGetLists } from "./util/lists";
 import type { List } from "../../../../prisma/generated/client";
 import { truncateCliTables } from "./util/truncateCliTables";
+import { apiPatchListMember } from "./util/listMember";
 
 describe("sendMail", () => {
   let defaultListId: string;
@@ -156,6 +157,60 @@ describe("sendMail", () => {
         const member = members[0];
         expect(member.status).toBe("subscribed");
       });
+
+      it("should not send an email to a member that is unsubscribed", async () => {
+        const email = "testDefaultListUnsubscribedUser@test.com";
+        const apiSendMailOpts = {
+          ...ApiSendMail.defaultFormData,
+          to: email,
+          listName: "default",
+          dangerouslyForceDeliver: true,
+        };
+        const { response: sendMailResponse } = await apiSendMail(
+          "testApiKey",
+          apiSendMailOpts
+        );
+        expect(sendMailResponse.status).toBe(200);
+
+        expect(await sendMailResponse.json()).toEqual({ result: "delivered!" });
+
+        // it should have subscribed the member to the default list
+        const member = await prisma.member.findUniqueOrThrow({
+          where: { listId_email: { listId: defaultListId, email } },
+        });
+
+        // unsubscribe the email
+        const { response: updateListMemberResponse } = await apiPatchListMember(
+          defaultListId,
+          member.id,
+          {
+            email,
+            status: "unsubscribed",
+          }
+        );
+
+        expect(updateListMemberResponse.status).toBe(200);
+
+        // try to send the email again
+        const { response: sendMailResponse2 } = await apiSendMail(
+          "testApiKey",
+          apiSendMailOpts
+        );
+        expect(sendMailResponse2.status).toBe(200);
+
+        expect(await sendMailResponse2.json()).toEqual({});
+
+        // it should not create a second message
+        const messages = await prisma.message.findMany();
+        expect(messages.length).toBe(1);
+
+        // the member should still be unsubscribed
+        const memberAgain = await prisma.member.findUniqueOrThrow({
+          where: { listId_email: { listId: defaultListId, email } },
+        });
+
+        expect(memberAgain.status).toBe("unsubscribed");
+      });
     });
 
     describe("sending to a different list than 'default'", () => {
@@ -166,12 +221,16 @@ describe("sendMail", () => {
 
       it("should send an email and create the list if necessary", async () => {
         const email = "testADifferentList@test.com";
-        const { response: sendMailResponse } = await apiSendMail("testApiKey", {
+        const apiSendMailOpts = {
           ...ApiSendMail.defaultFormData,
           to: email,
           listName: "mynewlist",
           dangerouslyForceDeliver: true,
-        });
+        };
+        const { response: sendMailResponse } = await apiSendMail(
+          "testApiKey",
+          apiSendMailOpts
+        );
         expect(sendMailResponse.status).toBe(200);
 
         expect(await sendMailResponse.json()).toEqual({ result: "delivered!" });
@@ -200,6 +259,31 @@ describe("sendMail", () => {
         const member = members[0];
         expect(member.email).toBe(email);
         expect(member.status).toBe("subscribed");
+
+        // Send a second email to the same list
+        // if the list exists it should not create it again
+        const { response: sendMailResponse2 } = await apiSendMail(
+          "testApiKey",
+          apiSendMailOpts
+        );
+        expect(sendMailResponse2.status).toBe(200);
+
+        expect(await sendMailResponse2.json()).toEqual({
+          result: "delivered!",
+        });
+
+        // it should have created a second message object
+
+        // it should not create another member object and the existing member object should still be subscribed
+        const membersAgain = await prisma.member.findMany({
+          where: { listId: list?.id },
+        });
+
+        expect(membersAgain.length).toBe(1);
+
+        const memberAgain = members[0];
+        expect(memberAgain.email).toBe(email);
+        expect(memberAgain.status).toBe("subscribed");
       });
     });
   });
