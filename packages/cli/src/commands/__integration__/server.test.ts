aa2cd64be5ee4e5adcae55ecf45903d178b5643d
Elliot Hursh <elliothursh@gmail.com>
2022-11-15 16:18:07 -0800

    update

---
+++ packages/cli/src/commands/__integration__/server.test.ts
@@ -0,0 +1,14 @@
+import { execCli } from "../__test__/execCli";
+
+jest.mock("../../util/log");
+
+describe("server command", () => {
+  describe("cli", () => {
+    it("builds", async () => {
+      const out = await execCli("server build");
+      expect(out).toContain("Compiled successfully");
+      expect(out).toContain("Finalizing page optimization...");
+      expect(out).toContain("First Load JS shared by all");
+    });
+  });
+});
