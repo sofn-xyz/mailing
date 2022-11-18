137121d1123caedd838848a782f61f36c443aefe
Alex Farrill <alex.farrill@gmail.com>
2022-11-17 21:09:01 -0800

    unsubscribe test

--- packages/cli/cypress/e2e/__integration__/unsubscribe.cy.integration.ts
+++ packages/cli/cypress/e2e/__integration__/unsubscribe.cy.integration.ts
@@ -60,32 +60,128 @@ describe("unsubscribe page", () => {
     });
 
     it("should show the right interface for multiple lists", function () {
-      cy.get("@defaultListMemberId").then((defaultListMemberId) => {
-        cy.visit(`/unsubscribe/${defaultListMemberId}`);
-        cy.get("h1").should("contain", "Email preferences");
+      // get aliases
+      const {
+        defaultListId,
+        defaultListMemberId,
+        anotherListId,
+        anotherListMemberId,
+      } = this;
+      expect(defaultListId).to.be.a("string");
+      expect(defaultListMemberId).to.be.a("string");
+      expect(anotherListId).to.be.a("string");
+      expect(anotherListMemberId).to.be.a("string");
 
-        // it should not show the nav
-        cy.get("nav").should("not.exist");
+      cy.visit(`/unsubscribe/${defaultListMemberId}`);
+      cy.get("h1").should("contain", "Email preferences");
 
-        // TODO: update this logic
-        // // check that the user is not unsubscribed
-        // cy.get("label").should("have.length", 1);
-        // cy.get("label").should("contain", "Unsubscribe from all emails");
-        // cy.get("input[type=checkbox]").should("have.length", 1);
-        // cy.get("input[type=checkbox]").should("not.be.checked");
-        // // check the unsubscribe all checkbox
-        // cy.get("input[type=checkbox]").check();
-        // // click the submit button
-        // cy.get("button").click();
-        // // should see "Saved!" message
-        // cy.get("div").should("contain", "Saved!");
-        // // the user's status should be unsubscribed
-        // cy.request({
-        //   url: `/api/lists/${defaultListMemberId}/members/${defaultListMemberId}`,
-        // }).then((response) => {
-        //   expect(response.status).to.eq(200);
-        //   expect(response.body.member.status).to.eq("unsubscribed");
-        // });
+      // it should not show the nav
+      cy.get("nav").should("not.exist");
+
+      // there should be 2 labels and checkboxes
+      cy.get("label").should("have.length", 2);
+      cy.get("input[type=checkbox]").should("have.length", 2);
+
+      // check the initial state of the form
+      // "anotherList" should be checked
+      cy.get("label")
+        .contains("anotherList")
+        .as("anotherListLabel")
+        .should("exist")
+        .siblings("input[type=checkbox]")
+        .should("be.checked");
+
+      // "defaultList" should be unchecked
+      cy.get("label")
+        .contains("Unsubscribe from all emails")
+        .as("defaultListLabel")
+        .should("exist")
+        .siblings("input[type=checkbox]")
+        .should("not.be.checked");
+
+      // check the "Unsubscribe all" checkbox
+      cy.get("@defaultListLabel").click();
+      cy.get("@defaultListLabel")
+        .siblings("input[type=checkbox]")
+        .should("be.checked");
+
+      // the "anotherList" checkbox should still be checked and disabled
+      cy.get("@anotherListLabel")
+        .siblings("input[type=checkbox]")
+        .should("be.checked")
+        .should("be.disabled");
+
+      // click the Save button
+      cy.get("button[type=submit]").contains("Save").click();
+
+      // should see "Saved!" message
+      cy.get(".form-success-message").should("contain", "Saved!");
+
+      // the user's status on the default list should be unsubscribed
+      cy.request({
+        url: `/api/lists/${defaultListId}/members/${defaultListMemberId}`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.member.status).to.eq("unsubscribed");
+      });
+
+      // the user's status on the "anotherList" list should be subscribed
+      cy.request({
+        url: `/api/lists/${anotherListId}/members/${anotherListMemberId}`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.member.status).to.eq("subscribed");
+      });
+
+      // reload the page
+      cy.visit(`/unsubscribe/${defaultListMemberId}`);
+      cy.get("h1").should("contain", "Email preferences");
+
+      // the initial state of the form should be the same as before
+      // the "Unsubscribe all" checkbox should be checked
+      cy.get("@defaultListLabel")
+        .siblings("input[type=checkbox]")
+        .should("be.checked");
+
+      // the "anotherList" checkbox should still be checked and disabled
+      cy.get("@anotherListLabel")
+        .siblings("input[type=checkbox]")
+        .should("be.checked")
+        .should("be.disabled");
+
+      // uncheck the "Unsubscribe all" checkbox
+      cy.get("@defaultListLabel").click();
+
+      // the "defaultList" checkbox should be unchecked
+      cy.get("@defaultListLabel")
+        .siblings("input[type=checkbox]")
+        .should("not.be.checked");
+
+      // the "anotherList" checkbox should be checked and enabled
+      cy.get("@anotherListLabel")
+        .siblings("input[type=checkbox]")
+        .should("be.checked");
+
+      // submit the form
+      cy.get("button[type=submit]").contains("Save").click();
+
+      // should see "Saved!" message
+      cy.get(".form-success-message").should("contain", "Saved!");
+
+      // the user's status on the default list should be subscribed
+      cy.request({
+        url: `/api/lists/${defaultListId}/members/${defaultListMemberId}`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.member.status).to.eq("subscribed");
+      });
+
+      // the user's status on the "anotherList" list should be subscribed
+      cy.request({
+        url: `/api/lists/${anotherListId}/members/${anotherListMemberId}`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.member.status).to.eq("subscribed");
       });
     });
   });
