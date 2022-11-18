7fd1374b47dc403cccc1ed54a1783a623210bf11
Monica Kogler <monica@campsh.com>
2022-11-18 11:28:07 -0800

    not even sure

--- packages/web/components/docs/DocsLayout.tsx
+++ packages/web/components/docs/DocsLayout.tsx
@@ -101,7 +101,7 @@ export default function DocsLayout({ children }) {
           )}
         >
           <h3 className="text-white text-2xl pt-7 mb-3">Docs</h3>
-          <button className="search border text-sm bg-gray-800 border-gray-800 text-slate-500 hover:text-blue w-full transition-all duration-300 text-left rounded-md px-3 pt-1 pb-[5px]">
+          <button className="search border text-sm bg-gray-800 border-gray-800 text-slate-500 hover:text-blue w-full transition-all duration-300 text-left rounded-md px-2 pt-1 pb-[5px]">
             <svg
               className="fill-slate-500 transition-all duration-300 inline -mt-[3px] mr-2"
               width="14"
