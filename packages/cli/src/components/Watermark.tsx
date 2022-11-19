6233e8ed651c074e109a2f32e7f2464820a0847b
Peter Sugihara <peter@campsh.com>
2022-11-19 12:04:14 -0800

    fix footer centering (next 13 bug?); add link to mailing.run

--- packages/cli/src/components/Watermark.tsx
+++ packages/cli/src/components/Watermark.tsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
 import LogoMarkSmall from "./icons/LogoMarkSmall";
 
 const Watermark: React.FC = () => {
   return (
-    <div className="pt-8 pb-12 text-center left-0 right-0 bottom-0 absolute mx-auto">
-      <LogoMarkSmall />
-      <p className="text-white text-[10px] pt-2 leading-none">
-        Powered by Mailing
-      </p>
+    <div className="pt-8 pb-12 flex justify-center left-0 right-0 bottom-0 absolute mx-auto">
+      <Link href="https://www.mailing.run" legacyBehavior>
+        <a
+          className="flex flex-col items-center"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LogoMarkSmall />
+          <div className="text-white text-[10px] pt-2 leading-none">
+            Powered by Mailing
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
