f5a9b6b40a4f4584601e11d6b2099faf875aae5c
Peter Sugihara <peter@campsh.com>
2022-11-23 22:04:21 -0800

    buttoning

--- packages/web/components/ui/KeyButton.tsx
+++ packages/web/components/ui/KeyButton.tsx
@@ -26,10 +26,12 @@ export default function KeyButton({
     >
       <div
         className={cx(
-          "hover:scale-9 -translate-y-2 -translate-x-2 transform rounded-2xl bg-green-200 transition-all duration-100 hover:-translate-y-1 hover:-translate-x-1 active:-translate-y-0 active:-translate-x-0 active:bg-green-100",
+          "hover:scale-9 transform rounded-2xl bg-green-200 transition-all duration-100 active:bg-green-100",
           {
-            "text-lg px-8 pt-4 pb-3": small,
-            "text-4xl px-16 pt-8 pb-7": !small,
+            "text-lg px-3 pt-2 pb-1 -translate-y-1 -translate-x-1 hover:-translate-y-0.5 hover:-translate-x-0.5 active:-translate-y-0 active:-translate-x-0":
+              small,
+            "text-4xl px-16 pt-8 pb-7 -translate-y-2 -translate-x-2 hover:-translate-y-1 hover:-translate-x-1 active:-translate-y-0 active:-translate-x-0":
+              !small,
           }
         )}
       >
