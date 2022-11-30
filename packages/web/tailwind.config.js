804a5f1463ce070d55a075962d0f92a58a863c8a
Peter Sugihara <peter@campsh.com>
2022-11-30 15:37:58 -0800

    only change fonts on web

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -7,6 +7,29 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme,
+  theme: {
+    ...theme,
+    extend: {
+      ...theme.extend,
+      fontFamily: {
+        ...theme.extend.fontFamily,
+        serif: ["swear-display-cilati", "Georgia", "Times New Roman", "serif"],
+        sans: [
+          "neue-haas-unica",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography")],
 };
