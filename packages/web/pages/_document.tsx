aa31e402f40cf92d44f53c91fef28b7a1ab0378f
Peter Sugihara <peter@campsh.com>
2022-11-30 15:20:59 -0800

    migth this fix

---
+++ packages/web/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/qqd8jtb.css" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
