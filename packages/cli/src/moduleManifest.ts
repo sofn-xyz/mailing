ab13f4e111ad7b421d167b946572d3460ff029ce
Elliot Hursh <elliothursh@gmail.com>
2022-12-02 01:15:22 -0800

    comment-out-module-manifest

--- packages/cli/src/moduleManifest.ts
+++ packages/cli/src/moduleManifest.ts
@@ -1,28 +1,7 @@
-import sendMail from "./emails";
-
-import AccountCreated from "./emails/AccountCreated";
-import NewSignIn from "./emails/NewSignIn";
-import Reservation from "./emails/Reservation";
-import ResetPassword from "./emails/ResetPassword";
-
-import * as AccountCreatedPreview from "./emails/previews/AccountCreated";
-import * as NewSignInPreview from "./emails/previews/NewSignIn";
-import * as ReservationPreview from "./emails/previews/Reservation";
-import * as ResetPasswordPreview from "./emails/previews/ResetPassword";
-
-const previews = {
-  AccountCreated: AccountCreatedPreview,
-  NewSignIn: NewSignInPreview,
-  Reservation: ReservationPreview,
-  ResetPassword: ResetPasswordPreview,
-};
-const templates = {
-  AccountCreated,
-  NewSignIn,
-  Reservation,
-  ResetPassword,
-};
-const config = { anonymousId: null };
+const sendMail = () => {};
+const previews: { [key: string]: any } = {};
+const templates: { [key: string]: any } = {};
+const config: { [key: string]: any } = {};
 
 export { sendMail, config, templates, previews };
 const moduleManifest = { sendMail, config, templates, previews };
