52043c1503e738a3d89811af4005aee836c632b5
Elliot Hursh <elliothursh@gmail.com>
2022-12-09 10:55:08 -0800

    pull over new templates

--- packages/cli/src/__mocks__/emails/previews/AccountCreated.tsx
+++ packages/cli/src/__mocks__/emails/previews/AccountCreated.tsx
@@ -4,8 +4,3 @@ import AccountCreated from "../AccountCreated";
 export function accountCreated() {
   return <AccountCreated name="Amelita" />;
 }
-
-
-export function accountCreatedJohn() {
-  return <AccountCreated name="John" />;
-}
