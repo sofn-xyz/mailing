87195779f8f0589a4df3281abfcfcd31953bd7d4
Alex Farrill <alex.farrill@gmail.com>
2023-03-08 11:32:04 -0800

    update create redwood app configs and bump node because redwood now requires >= 16.19.0

--- e2e/app/redwood_js.rb
+++ e2e/app/redwood_js.rb
@@ -12,7 +12,7 @@ module App
 
     def yarn_create!
       Dir.chdir(root_dir) do
-        system_quiet('yarn create redwood-app . --ts=false')
+        system_quiet('yarn create redwood-app . --ts=false --no-git')
 
         # yarn add peer dependencies
         system_quiet('yarn add next react react-dom')
