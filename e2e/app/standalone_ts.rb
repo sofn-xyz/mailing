ba2b9e4db5aac5aa77a049f3011670f88e1071c0
Alex Farrill <alex.farrill@gmail.com>
2023-03-07 20:44:12 -0800

    split standalone into standalone js and standalone ts

---
+++ e2e/app/standalone_ts.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module App
+  class StandaloneTs < Base
+    def initialize(root_dir, *args)
+      super('standalone', root_dir, *args)
+    end
+
+    private
+
+    def yarn_create!
+      Dir.chdir(root_dir) do
+        system_quiet('yarn init --yes')
+        system_quiet('npx tsc init --yes')
+
+        # yarn add peer dependencies
+        system_quiet('yarn add next react react-dom')
+      end
+    end
+  end
+end
