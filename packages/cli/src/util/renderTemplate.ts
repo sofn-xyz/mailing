7d9dfe72544e37fdd1119d5dc4a9ab5f0a1ccf41
Gabriel Samson <gabriel@slopepay.com>
2023-05-27 16:34:50 -0700

    Add subject to /api/render response

--- packages/cli/src/util/renderTemplate.ts
+++ packages/cli/src/util/renderTemplate.ts
@@ -1,18 +1,20 @@
 import React from "react";
 import { render } from "mailing-core";
 import { templates } from "../moduleManifest";
+import { getTemplateModule } from './moduleManifestUtil'
 
 type renderTemplateResult = {
   error?: string;
   mjmlErrors?: MjmlError[];
   html?: string;
+  subject?: string;
 };
 
 const renderTemplate = (
   templateName: string,
   props: { [key: string]: any }
 ): renderTemplateResult => {
-  const Template = templates[templateName as keyof typeof templates];
+  const Template = getTemplateModule(templateName)
   if (!Template) {
     return {
       error: `Template ${templateName} not found in list of templates: ${Object.keys(
@@ -21,7 +23,20 @@ const renderTemplate = (
     };
   }
 
-  return render(React.createElement(Template as any, props));
+  const { html, errors } = render(React.createElement(Template as any, props));
+
+  let subject
+  if (typeof Template.subject === "function") {
+    subject = Template.subject(props);
+  } else if (typeof Template.subject === "string") {
+    subject = Template.subject;
+  }
+
+  return {
+    html,
+    subject,
+    mjmlErrors: errors,
+  }
 };
 
 export default renderTemplate;
