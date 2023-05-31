ab6ba100741fc4272c2aee3c277395005482311a
Gabriel Samson <gabriel@slopepay.com>
2023-05-31 10:34:17 -0700

    Fix mjmlErrors assertion

--- packages/cli/src/util/__test__/renderTemplate.test.ts
+++ packages/cli/src/util/__test__/renderTemplate.test.ts
@@ -6,7 +6,7 @@ describe("renderTemplate", () => {
       name: "test",
     });
     expect(html).toBeUndefined();
-    expect(mjmlErrors).toBeUndefined();
+    expect(mjmlErrors).toEqual([]);
     expect(error).toMatch(/Template test not found in list of templates/);
   });
 
@@ -14,7 +14,7 @@ describe("renderTemplate", () => {
     const { error, mjmlErrors, html } = renderTemplate("AccountCreated", {
       name: "Test User",
     });
-    expect(mjmlErrors).toBeUndefined();
+    expect(mjmlErrors).toEqual([]);
     expect(error).toBeUndefined();
     expect(html).not.toBeUndefined();
     expect(html).toMatchSnapshot();
