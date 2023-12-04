9d78de69fc020035f8996a6c789c9debb0c53c95
psugihara <peter@campsh.com>
2023-12-04 12:20:53 -0800

    try unknown cast

--- packages/cli/src/util/tailwind.ts
+++ packages/cli/src/util/tailwind.ts
@@ -3,5 +3,8 @@ import tailwindConfig from "../../tailwind.config.js";
 
 const fullConfig = resolveConfig(tailwindConfig);
 
-export const colors = fullConfig.theme?.colors as Record<string, string>;
+export const colors = fullConfig.theme?.colors as unknown as Record<
+  string,
+  string
+>;
 export default fullConfig;
